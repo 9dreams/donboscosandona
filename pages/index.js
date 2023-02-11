b8207126b657c4c55177e3d41e9883b7be42af20
dottorgross <a.ros@donboscosandona.it>
2023-02-11 11:05:38 +0100

    Lettura dell'elenco degli articoli

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,40 @@
 import Head from 'next/head'
 import Layout from '/components/layout'
 
-export default function Home() {
+import { getDatiArticoli } from '/lib/articoli'
+
+export default function Home({ datiArticoli }) {
   return (
     <Layout>
       <Head>
       </Head>
 
-      <h1>Prova</h1>
+      <h2>Ultimi articoli</h2>
+
+      <ul>
+        {datiArticoli.map(
+          ({ id, date, title }) => (
+            <li>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}  
+              <br />
+            </li>
+          )
+        )}
+      </ul>
 
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const datiArticoli = getDatiArticoli();
+  return {
+    props: {
+      datiArticoli,
+    },
+  }
 }
