5d2ca404604aa38329cd89f6e9f22c19a5d183f7
dottorgross <a.ros@donboscosandona.it>
2023-03-05 10:36:40 +0100

    Esempi di denominazione dei file

---
+++ dev/dottorgross/Carousel.js
@@ -0,0 +1,7 @@
+export default function Carousel() {
+    return (
+        <footer>
+            <h2>Carousel</h2>
+        </footer>
+    )
+}
---
+++ pages/dev/dottorgross/index.js
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import Layout from '/components/layout'
+import Carousel from '/components/carousel'
+import Settori from '/components/settori'
+import Articoli from '/components/articoli'
+import Esplora from '/components/esplora'
+import Eventi from '/components/eventi'
+import PostInEvidenza from '/components/postInEvidenza'
+
+import { getDatiArticoli } from '/lib/articoli'
+
+let postInEvidenza = {
+  titolo: 'Le folli notti di Bilal',
+  descrizione: 'Il dott. Bilal, noto migliardario pieno di schèi, passa le sue notti tra belle ragazze e partite a poker. Come inviati di donboscosandona siamo riusciti ad accedere a questo mondo esclusivo per raccontarvelo in prima persona...',
+  immagine: 'https://media.istockphoto.com/id/522728130/it/foto/amici-divertirsi-giocando-a-poker.jpg?s=612x612&w=is&k=20&c=w2_p0SHrR_-cmURmZY9-dHXu3-qqVqN9QkdiZrgJpfc=',
+  testoImmagine: 'Bilal seduto su un divanetto in mezzo a due tipe.',
+  testoLink: 'Continua a leggere e scopri la cosa pazzesca che hanno fatto...',
+}
+
+export default function Home({ datiArticoli }) {
+  return (
+    <Layout>
+      <Head>
+      </Head>
+      <PostInEvidenza post={ postInEvidenza } />
+      <Carousel />
+      <Settori />
+      <Esplora />
+      <Eventi />
+      <Articoli dati={ datiArticoli } />
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const datiArticoli = getDatiArticoli();
+  return {
+    props: {
+      datiArticoli,
+    },
+  }
+}
