558bfa0b13a64b8adc0ea5c7b93086cb3799edf5
t.alves <t.alves@donboscosandona.it>
2023-03-13 11:04:44 +0100

    ojkwsed

--- dev/gerryscotti/Footer.js
+++ dev/gerryscotti/Footer.js
@@ -5,12 +5,13 @@ import Link from '@mui/material/Link'
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Card from '@mui/material/Card'
+import Toolbar from '@mui/material/Toolbar'
 
 
-
-export default function Footer() {
+export default function TheFooter(props) {
     return (
         <Container maxWidth= {true}>
+            
            <Box
                  sx={{
                     position: 'absolute',
@@ -18,11 +19,112 @@ export default function Footer() {
                     bottom: 0,
                     right: 0,
                     left: 0,
-                    backgroundColor: 'rgba(0,0,0,0.9)', }}
-           
+                    backgroundColor: props.color,
+               
+                    
+                }}
            >
+             <Grid container>
+                    <Grid item md={4}>
+                        <Box
+                            sx={{
+                                position: 'relative',
+                                p: { xs: 3, md: 6 },
+                                pr: { md: 0 },
+                            }}
+                        >
+                           
+                            <Typography component="h1" color="White" >
+                                {props.title1}
+                            </Typography>
+                            <Typography component="h5" color="darkgrey" paragraph>
+                                {props.description1}
+                            </Typography>
+                        
+                        
+                            <Box
+                            sx={{
+                                position: 'assolute',
+                                p: { xs: 0, md: 0},
+                                pr: { md: 0 },
+                            }}
+                                >
+                             <Typography component="h1" color="White" >
+                                {props.title2}
+                            </Typography>
+                                </Box>
+
+                        </Box>
+                    </Grid>
+
+                    <Grid item md={4}>
+                        <Box
+                            sx={{
+                                position: 'relative',
+                                p: { xs: 3, md: 6 },
+                                pr: { md: 0 },
+                            }}
+                        >
+                           
+                            <Typography component="h1" color="White" >
+                                {props.title1}
+                            </Typography>
+                            <Typography component="h5" color="darkgrey" paragraph>
+                                {props.description1}
+                            </Typography>
+                        
+                        
+                            <Box
+                            sx={{
+                                position: 'assolute',
+                                p: { xs: 0, md: 0},
+                                pr: { md: 0},
+                            }}
+                                >
+                             <Typography component="h1" color="White" >
+                                {props.title2}
+                            </Typography>
+                                </Box>
+
+                        </Box>
+                    </Grid>
+
+                    <Grid item md={4}>
+                        <Box
+                            sx={{
+                                position: 'relative',
+                                p: { xs: 3, md: 6 },
+                                pr: { md: 0 },
+                            }}
+                        >
+                           
+                            <Typography component="h1" color="White" >
+                                {props.title1}
+                            </Typography>
+                            <Typography component="h5" color="darkgrey" paragraph>
+                                {props.description1}
+                            </Typography>
+                        
+                        
+                            <Box
+                            sx={{
+                                position: 'assolute',
+                                p: { xs: 0, md: 0},
+                                pr: { md: 0 },
+                            }}
+                                >
+                             <Typography component="h1" color="White" >
+                                {props.title2}
+                            </Typography>
+                                </Box>
+
+                        </Box>
+                    </Grid>
+                </Grid>
+           
 
            </Box>
+           
         </Container>
     )
 }
--- pages/dev/gerryscotti/index.js
+++ pages/dev/gerryscotti/index.js
@@ -14,31 +14,43 @@ import Footer from '/dev/gerryscotti/Footer'
 
 
 
-import { MyFooter } from 'dev/gerryscotti/Footer'
+import { TheFooter } from 'dev/gerryscotti/Footer'
 
 let socials = [
-    { title: 'Facebook', imageUrl: 'https...', url: '...' },
-    { title: 'Instagram', imageUrl: 'https...', url: '...' },
-    { title: 'Youtube', imageUrl: 'https...', url: '...' },
-    { title: 'Linkedin', imageUrl: 'https...', url: '...' },
-  ]
-  
-  let images = [
-    { title: 'Informatico', imageUrl: 'https...', url: '...' },
-    { title: 'Informatico', imageUrl: 'https...', url: '...' },
-    { title: 'Informatico', imageUrl: 'https...', url: '...' },
-  ]
-  
-  let menu = [
-      { title: 'BLOG', url: '/informatico' },
-      { title: 'PRESENTATION', url: '/energie' },
-      { title: 'DISCOVER', url: '/elettrico' },
-      { title: 'PAYMENT', url: '/meccanico' },
-      { title: 'CONTACT US', url: '/motoristico' },
-  ]
-  export default function Pagina ({ MyFooter }) {
-    return (
-        <Footer />
-    )
-  }
-  
+  { title: 'Facebook', imageUrl: 'https...', url: '...' },
+  { title: 'Instagram', imageUrl: 'https...', url: '...' },
+  { title: 'Youtube', imageUrl: 'https...', url: '...' },
+  { title: 'Linkedin', imageUrl: 'https...', url: '...' },
+]
+
+let images = [
+  { title: 'Informatico', imageUrl: 'https...', url: '...' },
+  { title: 'Informatico', imageUrl: 'https...', url: '...' },
+  { title: 'Informatico', imageUrl: 'https...', url: '...' },
+]
+
+let menu = [
+  { title: 'BLOG', url: '/informatico' },
+  { title: 'PRESENTATION', url: '/energie' },
+  { title: 'DISCOVER', url: '/elettrico' },
+  { title: 'PAYMENT', url: '/meccanico' },
+  { title: 'CONTACT US', url: '/motoristico' },
+]
+export default function Pagina({ TheFooter }) {
+  return (
+    <Footer
+      imageUrl="https://immagine di sfondo"
+      color="#777"
+      opacitycolor="1"
+      opacity={1}
+      title1="About Us"
+      description1="Creative Tim is a startup..."
+      title2="Social Feed"
+      socials={socials}
+      title3="I Settori"
+      images={images}
+      menu={menu}
+      copyright="Copyright (C) 2023 9dreams Agency."
+    />
+  )
+}
