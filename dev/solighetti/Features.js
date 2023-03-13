d3632805ddaa3e3497ff95c3e9cd26a03b7aca5c
Alessio Solighetto <a.solighetto@donboscosandona.it>
2023-03-13 11:03:01 +0100

    update

--- dev/solighetti/Features.js
+++ dev/solighetti/Features.js
@@ -1,37 +1,18 @@
 import { Grid } from '@mui/material';
 import styles from './Features.module.css';
 
-const HomePage = (props) => {
-  const { title, description, Props } = props;
+const Features = (props) => {
+  const { title, description, features, cardWidth } = props;
 
-  const features = [
-    {
-      id: 1,
-      image: "https://i.ibb.co/JjgJ35G/comment.png",
-      title: "Chi siamo",
-      description: "Una breve descrizione di chi siamo."
-    },
-    {
-      id: 2,
-      image: "https://i.ibb.co/LNkL1hY/shield.png",
-      title: "Cosa facciamo",
-      description: "Una breve descrizione di cosa facciamo"
-    },
-    {
-      id: 3,
-      image: "https://i.ibb.co/f4sGXQp/fingerprint.png",
-      title: "Contattaci",
-      description: "I nostri contatti"
-    }
-  ];
+
 
   return (
-    <div className={`${styles.container} text-center`} {...Props}>
+    <div className={`${styles.container} text-center`}>
       <h1 className={`${styles.title}`}>{title}</h1>
       <p className={`${styles.description}`}>{description}</p>
       <Grid container spacing={2} className={styles.grid}>
         {features.map((feature) => (
-          <Grid key={feature.id} item xs={12} sm={6} md={4} lg={4}>
+          <Grid key={feature.id} item xs={12} lg={cardWidth}>
             <div className={styles.feature}>
               <img src={feature.image} alt={feature.title} width="100" height="100" />
               <h2>{feature.title}</h2>
@@ -44,9 +25,9 @@ const HomePage = (props) => {
   );
 };
 
-HomePage.defaultProps = {
+Features.defaultProps = {
   title: 'Features',
   description: 'Questo è un paragrafo',
 };
 
-export default HomePage;
+export default Features;
--- pages/dev/solighetti/index.js
+++ pages/dev/solighetti/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import Features from 'dev/solighetti/Features';
 
-const featuresData = [
+const features = [
   {
-
+    id: 1,
+    image: "https://i.ibb.co/JjgJ35G/comment.png",
+    title: "Chi siamo",
+    description: "Una breve descrizione di chi siamo."
   },
-
-];
+  {
+    id: 2,
+    image: "https://i.ibb.co/LNkL1hY/shield.png",
+    title: "Cosa facciamo",
+    description: "Una breve descrizione di cosa facciamo"
+  },
+  {
+    id: 3,
+    image: "https://i.ibb.co/f4sGXQp/fingerprint.png",
+    title: "Contattaci",
+    description: "I nostri contatti"
+  }
+]
 
 function HomePage() {
   return (
     <div>
-      <Features />
+      <Features
+        title="I nostri punti di forza"
+        description="This is the paragraph......."
+        features={features}
+        cardWidth={4}
+      />
     </div>
   );
 }
