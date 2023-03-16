c9db732536c86cf6adfc9351c1f64990269f7a36
Valentina Segato <v.segato@donboscosandona.it>
2023-03-16 10:46:27 +0100

    commit mio

--- dev/fv/LandingHero.js
+++ dev/fv/LandingHero.js
@@ -6,98 +6,123 @@ import Link from 'next/link'
 import Paper from '@mui/material/Paper'
 import styles from 'dev/fv/landing.module.css'
 import Toolbar from '@mui/material/Toolbar'
+import Container from '@mui/material/Container'
 
 export default function LandingHero(props) {
     return (
-        <Paper
+        <Container
+            maxWidth
+            disableGutters={true}
             sx={{
-                position: 'relative',
-                backgroundColor: 'grey.800',
-                color: '#fff',
-                mb: 4,
-                backgroundImage: 'url(' + props.imageUrl + ')',
-                backgroundSize: 'cover',
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center',
-                height: '90vh',
-                
-            }}
-            elevation={5}
-        >
-            <Box
+                height: '100vh',
+                padding: 'ovh',
+                margin: '0vh',
+                width: '100%'
+            }}>
+            <Paper
                 sx={{
-                    position: 'absolute',
-                    top: 0,
-                    bottom: 0,
-                    right: 0,
-                    left: 0,
-                    backgroundColor: 'rgba(0,0,0,0.4)',
-                }}
-            />
-            <Grid container>
-                <Grid item md={2}>
-                    <Box
-                        sx={{
-                            position: 'relative',
-                            p: { xs: 3, md: 6 },
-                            pr: { md: 0 },
-                        }}
-                    >
-                        <Typography component="h2" variant="h6" color="inherit" gutterBottom>
-                            {props.siteName}
-                        </Typography>
+                    position: 'relative',
+                    color: '#fff',
+                    mb: 4,
+                    height: '100vh',
+                    backgroundImage: 'url(' + props.imageUrl + ')',
+                    padding: 0,
+                    margin: 0,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover', 
+                    backgroundPosition: 'center',
 
-                    </Box>
-                </Grid>
-                <Grid item md={5}>
-                    <Box
-                        sx={{
-                            position: 'relative',
-                            p: { xs: 3, md: 6 },
-                            pr: { md: 0 },
-                            top: 290,
-                        }}
-                    >
-                        <Typography component="h1" variant="h3" color="inherit" gutterBottom>
-                            {props.title}
-                        </Typography>
-                        <Typography component="h5" color="inherit" paragraph>
-                            {props.description}
-                        </Typography>
-                        <Button variant="subtitle1" href={props.buttonUrl} className={styles.btLanding} >{props.buttonText}</Button>
-                    </Box>
-                </Grid>
-                <Grid item md={5}>
-                    <Box
-                        sx={{
-                            position: 'relative',
-                            p: { xs: 3, md: 6 },
-                            pr: { md: 0 },
-                        }}
-                    >
-                        <Toolbar
-                            component="nav"
-                            variant="dense"
-                            sx={{ justifycontent: 'space-between', overflowX: 'auto' }}
+                }}
+                elevation={5}
+            >
+                <Box
+                    sx={{
+                        position: 'absolute',
+                        top: 0,
+                        bottom: 0,
+                        right: 0,
+                        left: 0,
+                        backgroundColor: 'rgba(0,0,0,0.4)',
+                    }}
+                />
+                <Grid container >
+                    <Grid item md={6} xs={8} >
+                        <Box
+                            sx={{
+                                position: 'relative',
+                                p: { xs: 3, md: 6 },
+                                pr: { md: 0 },
+                                top: 55,
+                            }}
                         >
-{
-                            props.menu.map((settore) => (
-                                <Link className={styles.link_settori}
-                                    color="inherit"
-                                    noWrap
-                                    key={settore.title}
-                                    variant="body2"
-                                    href={settore.url}
-                                    sx={{ p: 1, flexShrink: 0 }}
-                                >
-                                    {settore.title}
-                                </Link>
-                            ))
-                        }
-                        </Toolbar>
-                    </Box>
+                            <Typography component="h2" variant="h6" color="inherit" gutterBottom>
+                                {props.siteName}
+                            </Typography>
+
+                        </Box>
+                    </Grid>
+                    <Grid item md={6} className={styles.nav}>
+                        <Box
+                            sx={{
+                                position: 'relative',
+                                p: { xs: 2, md: 5 },
+                                pr: { md: 0 },
+                                top: 55,
+                                overflow: 'auto',
+                                width: '100%',
+                            }}
+                        >
+                            <Toolbar
+                                component="nav"
+                                variant="dense"
+                                sx={{
+                                    justifycontent: 'space-between',
+                                    overflowX: 'auto',
+                                    overflow: 'hidden',
+                                    overflowWrap: 'break-word',
+                                }}
+                            >
+                                {
+                                    props.menu.map((settore) => (
+                                        <Link className={styles.link_settori}
+                                            color="inherit"
+                                            noWrap
+                                            key={settore.title}
+                                            variant="body2"
+                                            href={settore.url}
+                                            sx={{ p: 1, flexShrink: 0 }}
+                                        >
+                                            {settore.title}
+                                        </Link>
+                                    ))
+                                }
+                            </Toolbar>
+                        </Box>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Paper>
+                <Grid container>
+                    <Grid item md={2} />
+                    <Grid item md={5}>
+                        <Box
+                            sx={{
+                                position: 'relative',
+                                p: { xs: 3, md: 6 },
+                                pr: { md: 0 },
+                                top: 130,
+                            }}
+                        >
+                            <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+                                {props.title}
+                            </Typography>
+                            <Typography component="h5" color="inherit" paragraph>
+                                {props.description}
+                            </Typography>
+                            <Button variant="subtitle1" href={props.buttonUrl} className={styles.btLanding} >{props.buttonText}</Button>
+                        </Box>
+                    </Grid>
+                </Grid>
+
+            </Paper>
+        </Container>
     )
 }
