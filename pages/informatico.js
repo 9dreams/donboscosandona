b901dfd4873caf1dd672eadba96cef0907f956cb
Alessio Solighetto <a.solighetto@donboscosandona.it>
2023-03-20 10:59:11 +0100

    iniziata pagina informatica

---
+++ pages/informatico.js
@@ -0,0 +1,81 @@
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+
+import Head from 'next/head'
+import Image from 'next/image'
+import Layout from '/components/Layout'
+import LandingHero from '/components/LandingHero.js'
+import Features from '/components/Features'
+import Carousel from '/components/Carousel'
+import PostInEvidenza from '/components/PostInEvidenza'
+import Products from '/components/Products'
+import Testimonials from '/components/Testimonials'
+import Team from '/components/Team'
+import Post from '/components/Post'
+
+
+
+
+let menu = [
+    { title: 'Chi siamo', url: '/chi-siamo' },
+    { title: 'Contatti', url: '/contatti' },
+    { title: 'Dove siamo', url: '/dove-siamo' },
+    { title: 'Trasparenza', url: '/trasparenza' },
+]
+
+let slides = [
+    {
+        titolo: 'Fatti un bel giro',
+        descrizione: 'Scopri tutti i segreti del nostro Centro con il tour virtuale!',
+        immagine: '/images/informatico/carousel1.jpg'
+    },
+    {
+        titolo: 'Progetto GIVE',
+        descrizione: "Il 15 e 16 maggio il nostro Centro ospiterà l'edizione 2023 di GIVE (GROHE Installer Vocational Training and Education), il programma di formazione dedicato ai giovani installatori idraulici: un ambiente dedicato al training pratico proposto dai professionisti di GROHE.",
+        immagine: '/images/informatico/carousel2.jpg'
+    },
+    {
+        titolo: 'Concorso nazionale settore elettrico',
+        descrizione: "A maggio il nostro Centro avrà l'onore di ospitare il Concorso Nazionale del Settore Elettrico: tutti i Centri di Formazione Professionale salesiani d'Italia invieranno i loro campioni per una settimana di sfida e condivisione professionale...",
+        immagine: '/images/informatico/carousel3.jpg'
+    },
+]
+
+let slides2 = slides
+
+export default function Home({ datiArticoli }) {
+    return (
+        <Layout>
+            <Head />
+            {
+
+            }
+            <LandingHero
+                opacity={0.5}
+                siteName="CFP DON BOSCO"
+                title="Settore Informatico"
+                description="Diventa adulto con il metodo educativo di don Bosco preparandoti ad entrare a testa alta nel mondo professionale di domani!"
+                buttonUrl={'https://youtu.be/0Fh458fo8SQ'}
+                buttonText="Guarda il video"
+                imageUrl="/images/informatico/settore-informatico.jpg"
+                menu={menu}
+            />
+            <Carousel slides={slides} maxWidth={false} />
+
+            
+            <Grid container spacing={12}>
+            <Grid item xs={4}>
+                <Image src='/images/informatico/9dreams.png' width={350} height={132} />
+            </Grid>
+                <Grid item xs={4}>
+                    <Image src='/images/informatico/developer.png' width={350} height={132} />
+                </Grid>
+                <Grid item xs={4}>
+                    <Image src='/images/informatico/youtube.png' width={350} height={132} />
+                </Grid>
+            
+        </Grid>
+
+        </Layout>
+    )
+}
