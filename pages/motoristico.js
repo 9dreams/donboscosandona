8b217516103e772f2e5a6419ab67b76d6ee813e2
Eros Simoncin <e.simoncin@donboscosandona.it>
2023-03-20 09:04:23 +0100

    inizio

---
+++ pages/motoristico.js
@@ -0,0 +1,16 @@
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+
+import Head from 'next/head'
+import Layout from '/components/Layout'
+import LandingHero from '/components/LandingHero2.js'
+import Features from '/components/Features'
+import Carousel from '/components/Carousel'
+import PostInEvidenza from '/components/PostInEvidenza'
+import Products from '/components/Products'
+import Testimonials from '/components/Testimonials'
+import Team from '/components/Team'
+import Post from '/components/Post'
+
+import { getDatiArticoli } from '/lib/articoli'
+
