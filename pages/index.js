cfa506a62f21115972c39b5a4017bb4c82f821a5
Filippo Urban <f.urban@donboscosandona.it>
2023-03-23 09:46:37 +0100

    link messi su products

--- pages/index.js
+++ pages/index.js
@@ -76,6 +76,8 @@ let members = [
     description: "And I love you like Kanye",
     description1: "loves Kanye. We need to restart the human foundation",
     imageUrl: "https://media.gamerbrain.net/wp-content/uploads/2019/11/27094442/anima.jpg",
+    email:"c.coso@donboscosandona.it",
+    phone: '0421 3388949874950',
   },
   {
     name: "Alessandro Cappelletto",
@@ -83,6 +85,8 @@ let members = [
     description: "And I love you like Kanye",
     description1: "loves Kanye. We need to restart the human foundation",
     imageUrl: "https://preview.redd.it/6ymh125pcra71.jpg?auto=webp&s=d0ec0c4112ca7a2a8f8228be3dfda0645091ac02",
+    email:"c.ciaoooo@donboscosandona.it",
+    phone: '123456',
   },
   {
     name: "Alessandro Ferro",
@@ -90,7 +94,9 @@ let members = [
     description: "And I love you like Kanye",
     description1: "loves Kanye. We need to restart the human foundation",
     imageUrl: "https://media.licdn.com/dms/image/C5603AQHlyDoD0W6G-A/profile-displayphoto-shrink_800_800/0/1551801387517?e=2147483647&v=beta&t=JT3YTFL7g1ykrUcSwl7W0_iO0YSiBolctjntN2T9dCA",
-    linkedinUrl: "https://it.linkedin.com/in/alessandro-ferro-128aab103",
+ 
+    email:"a.ferro@donboscosandona.it",
+    phone: '0421 111 222',
   },
   {
     name: "Anna Maria D'Ambrosio",
@@ -98,6 +104,8 @@ let members = [
     description: "And I love you like Kanye",
     description1: "loves Kanye. We need to restart the human foundation",
     imageUrl: "https://www.webboh.it/wp-content/uploads/2020/10/Surry-758x424.jpg",
+    email:"l.hdggfd@donboscosandona.it",
+    phone: '12365598445',
   },
   {
     name: "Roberta Toffoletto e Alessia Zanin",
@@ -105,6 +113,8 @@ let members = [
     description: "And I love you like Kanye",
     description1: "loves Kanye. We need to restart the human foundation",
     imageUrl: "https://www.webboh.it/wp-content/uploads/2020/10/Surry-758x424.jpg",
+    email:"c.cosocoso@donboscosandona.it",
+    phone: '0421 123456338 950',
   },
   {
     name: "Andrea Pasqualetto",
@@ -112,6 +122,8 @@ let members = [
     description: "And I love you like Kanye",
     description1: "loves Kanye. We need to restart the human foundation",
     imageUrl: "https://www.webboh.it/wp-content/uploads/2020/10/Surry-758x424.jpg",
+    email:"c.cosinocoso@donboscosandona.it",
+    phone: '123456 338 950',
   },
   {
     name: "Francesco Cicogna",
@@ -119,6 +131,8 @@ let members = [
     description: "And I love you like Kanye",
     description1: "loves Kanye. We need to restart the human foundation",
     imageUrl: "https://preview.redd.it/6ymh125pcra71.jpg?auto=webp&s=d0ec0c4112ca7a2a8f8228be3dfda0645091ac02",
+    email:"hhh.coso@donboscosandona.it",
+    phone: '0421 338 950',
   },
   {
     name: "Martina Dianese",
@@ -126,6 +140,8 @@ let members = [
     description: "And I love you like Kanye",
     description1: "loves Kanye. We need to restart the human foundation",
     imageUrl: "https://media.licdn.com/dms/image/C5603AQHlyDoD0W6G-A/profile-displayphoto-shrink_800_800/0/1551801387517?e=2147483647&v=beta&t=JT3YTFL7g1ykrUcSwl7W0_iO0YSiBolctjntN2T9dCA",
+    email:"c.coso@donboscosandona.it",
+    phone: '0421 338 950',
   },
 ]
 
@@ -134,31 +150,36 @@ let settori = [
     title: "Elettrico",
     category: "QUALIFICA DI OPERATORE ELETTRICO",
     description: "Installa ed effettua la manutenzione di impianti civili e industriali, sia con tecnologie tradizionali che automatizzate. Realizza la programmazione dei componenti domotici e dei controllori programmabili industriali.",
-    immagine: "https://www.donboscosandona.it/img/column/42480c58c90ada8ff75c443fdde83ae71fea75ac.jpg?1539529685"
+    immagine: "https://www.donboscosandona.it/img/column/42480c58c90ada8ff75c443fdde83ae71fea75ac.jpg?1539529685",
+    link: "https://www.donboscosandona.it/elettrico"
   },
   {
     title: "Energia",
     category: "QUALIFICA DI OPERATORE DI IMPIANTI TERMO-IDRAULICI",
     description: "Interviene nell'installazione, collaudo e manutenzione di impianti termici, idraulici, di condizionamento e fotovoltaici, con una particolare attenzione al risparmio energetico, anche grazie alla building automation.",
-    immagine: "https://www.donboscosandona.it/img/column/417afaa0f0697f0f0aabf9ccf61030769cb76f14.jpg?1539529180"
+    immagine: "https://www.donboscosandona.it/img/column/417afaa0f0697f0f0aabf9ccf61030769cb76f14.jpg?1539529180",
+    link: "https://www.donboscosandona.it/energia"
   },
   {
     title: "Informatico",
     category: "DIPLOMA DI TECNICO INFORMATICO",
     description: "Installa e configura hardware e software, esegue la manutenzione di sistemi, reti e terminali utente, fornisce assistenza tecnica, effettua l'elaborazione e la manutenzione di dati su archivi digitali. Con il quarto anno in Sistema Duale diventa tecnico sviluppatore di Soluzioni Software.",
-    immagine: "https://www.donboscosandona.it/img/column/97f264e9d16a0856dd2cb973630af1cac4426f73.jpg?1539529314"
+    immagine: "https://www.donboscosandona.it/img/column/97f264e9d16a0856dd2cb973630af1cac4426f73.jpg?1539529314",
+    link: "https://www.donboscosandona.it/informatico"
   },
   {
     title: "Meccanico",
     category: "DIPLOMA DI TECNICO PER LA CONDUZIONE E LA MANUTENZIONE DI IMPIANTI AUTOMATIZZATI",
     description: "Realizza lavorazioni al tornio, fresatrice e macchine utensili in generale. Partendo dal progetto realizzato con il CAD effettua la programmazione delle macchine a controllo numerico. Realizza saldature ad arco, TIG, MAG, taglio al plasma.",
-    immagine: "https://www.donboscosandona.it/img/column/b0bd034769ac3471bf375239f1e64e39f5684aa6.jpg?1539523300"
+    immagine: "https://www.donboscosandona.it/img/column/b0bd034769ac3471bf375239f1e64e39f5684aa6.jpg?1539523300",
+    link: "https://www.donboscosandona.it/meccanico"
   },
   {
     title: "Motoristico / Carrozzeria",
     category: "QUALIFICA DI OPERATORE ALLA RIPARAZIONE DI VEICOLI A MOTORE",
     description: "Effettua riparazioni e manutenzione dei motori e dell'autoveicolo in tutte le sue parti. Effettua diagnosi e prove sui gas di scarico intervenendo per risolvere le anomalie.",
-    immagine: "https://www.donboscosandona.it/img/column/342f349c6bb5bbf9a37b95353df2bee2309b9792.jpg?1539524741"
+    immagine: "https://www.donboscosandona.it/img/column/342f349c6bb5bbf9a37b95353df2bee2309b9792.jpg?1539524741",
+    link: "https://www.donboscosandona.it/motoristico"
   },
 ]
 
@@ -172,16 +193,16 @@ let postInEvidenza = {
 
 const testimonials = [
   {
-    name: "Gigi Thompson",
-    image: "https://i.ibb.co/4MQnQw0/img1.jpg",
-    text: "Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione ",
-    social: "@GIGITHOMPSON"
+    name: "Michele Venturato",
+    image: "https://i.postimg.cc/3NMjxmcp/Screenshot-2023-03-19-alle-16-36-15.png",
+    text: "Grande scuola. Una vera scuola professionale e morale. Sforna ogni anno moltissimi tecnici aggiornati con I tempi e in grado di integrarsi con facilita' nel mondo del lavoro. Complimenti a tutto il sistema.",
+    social: "@Google"
   },
   {
-    image: "https://i.ibb.co/QPG6j9b/img2.jpg",
-    name: "Gina Andrew",
-    text: "Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione ",
-    social: "@GINAANDREW"
+    image: "/images/home/recensioni/bob.png",
+    name: "Daniele Mladenovic",
+    text: "Ho trascorso alcuni anni presso il CFP e ho trovato l'ambiente molto accogliente e confortevole, grazie alla disponibilità di tutto il personale. In particolare, ho apprezzato la passione degli insegnanti, che si sono dimostrati molto competenti. Inoltre, ho avuto l'opportunità di partecipare ad attività extracurricolari e progetti che mi hanno permesso di sviluppare le mie capacità. Infine, consiglio questo istituto sottolineando l'importanza dello sviluppo e della scoperta dei propri talenti che è ciò che ci rende unici.",
+    social: "@BOBTEK"
   },
   {
     image: "https://i.ibb.co/dpQrpNt/img3.jpg",
