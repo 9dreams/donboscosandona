1337fa6baa0571f2e87f8789f9391da9acb73b52
Valentina Segato <v.segato@donboscosandona.it>
2023-03-23 11:03:23 +0100

    creazione file

---
+++ pages/job.js
@@ -0,0 +1,13 @@
+import LandingHero from "@/components/LandingHero2";
+import Layout from "@/components/Layout";
+
+
+
+export default function Home() {
+    return (
+      <Layout>
+
+      </Layout>
+    )
+  }
+  
