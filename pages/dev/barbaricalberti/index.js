5949d30a91c012fc54ae484429ee55cf62da34dc
dottorgross <a.ros@donboscosandona.it>
2023-03-26 17:25:55 +0200

    Sistemazione file barbaricalberti

--- pages/dev/barbaricalberti/index.js
+++ pages/dev/barbaricalberti/index.js
@@ -1,4 +1,5 @@
-import LandingHero from "/dev/barbaricalbertic/LandingHero"
+import LandingHero from "/components/LandingHero2"
+import Products from "/components/Products"
 
 let menu = [
     { title: 'Informatico', url: '/informatico' },
@@ -9,25 +10,12 @@ let menu = [
 export default function Pages() {
     return (
         <>
-            <Container sx={{ alignContent: 'center', height: '100vh' }}>
-                <center>
-                    <br />
-                    <h1>Some of Our Aweson Products - 2</h1>
-                </center>
-
-                <center>
-                    <Typography variant="subtitle1" sx={{ color: 'grey' }}>This is the paragraph where you can write more details about your projects. Keep you user <br /> engaged providing meaningful information</Typography>
-                </center>
-
-                <Box marginTop="90px" display="flex" flexWrap="wrap" sx={{ p: { xs: 3, md: 6 }, pr: { md: 0 } }} products={products} >
-                    <Products
-                        title="Some of Our Awesome Products"
-                        description="This is the paragraph......."
-                        cardWidth={4}
-                        products={products}
-                    />
-                </Box>
-            </Container>
+            <Products
+                title="Some of Our Awesome Products"
+                description="This is the paragraph......."
+                cardWidth={4}
+                products={products}
+            />
         </>
     )
 }
@@ -38,21 +26,21 @@ const products = [
         category: "FREE UI KIT",
         description: "It is a Fre Material-UI Kit with.....",
         immagineUrl: "https://www.tmedia.it/2019/wp-content/uploads/2021/07/spletna-tmedia-04-500x500.png",
-        url:"https://books.donboscosandona.it/containers/117?pagina_id=462"
+        url: "https://books.donboscosandona.it/containers/117?pagina_id=462"
     },
     {
         title: "NextJS Material Kit Free",
         category: "FREE UI KIT",
         description: "It is a Fre Material-UI Kit with.....",
         immagineUrl: "https://www.smartworld.it/wp-content/uploads/mobile/2018/10/apple-ipad-pro-2018-download-wallpaper-8-500x500.jpg",
-        url:"https://books.donboscosandona.it/containers/117?pagina_id=462"
+        url: "https://books.donboscosandona.it/containers/117?pagina_id=462"
     },
     {
         title: "NextJS Material Kit Free",
         category: "FREE UI KIT",
         description: "It is a Fre Material-UI Kit with.....",
         immagineUrl: "https://www.tec-artigrafiche.it/wp-content/uploads/2019/12/Lion-.-1600x1115_stampa-500x500.jpg",
-        url:"https://books.donboscosandona.it/containers/117?pagina_id=462"
+        url: "https://books.donboscosandona.it/containers/117?pagina_id=462"
     },
 ]
 
