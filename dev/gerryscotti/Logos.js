aed2f3a0231cb54abf2da1aa43f4911972a25e6c
t.alves <t.alves@donboscosandona.it>
2023-03-27 10:12:25 +0200

    fine logo

--- dev/gerryscotti/Logos.js
+++ dev/gerryscotti/Logos.js
@@ -7,29 +7,51 @@ import Container from '@mui/material/Container'
 
 export default function TheLogo(props) {
     return (
-        <Container>
+        <Container >
             <Box
                 sx={{
 
-                    left: 500,
+
                     backgroundColor: props.color,
                 }}
             >
                 <Grid container
 
                 >
-                    <Grid item  xs={0}>
+                    <Grid item xs={12} >
+                        <Box
+                            sx={{
+                                position: 'relative',
+                                p: { xs: 0, md: 0 },
+                                pr: { md: 0 },
+                                left: 700,
+                            }}
+                        >
 
-                   
-                   
-                    
+
+
+
+                            <img src={props.sponsorUrl} width="55%" />
+
+
+
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12}>
+                    <Box
+                            sx={{
+                                position: 'relative',
+                                p: { xs: 1, md: 2 },
+                                pr: { md: 0 },
+                                left: 1,
+                            }}
+                        >
                         <img src={props.logoUrl} width="20%" />
-                      
-                        <img src={props.sponsorUrl} width="50%" />
-                     
-                        </Grid>
-                    
+                        </Box>
+                    </Grid>
+
                 </Grid>
+
 
             </Box>
         </Container>
