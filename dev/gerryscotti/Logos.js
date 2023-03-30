a0140d4aed59093d485d1e95ea2cc2ac46995f21
t.alves <t.alves@donboscosandona.it>
2023-03-30 09:34:43 +0200

    end lavoro

--- dev/gerryscotti/Logos.js
+++ dev/gerryscotti/Logos.js
@@ -7,88 +7,32 @@ import Link from '@mui/material/Link'
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 
+
 export default function TheLogo(props) {
     return (
-        <Container >
-            <Box
-                sx={{
+        <Container maxWidth={true} backgroundColor = "props.color">
+
+            
 
 
-                    backgroundColor: props.color,
+            <Grid container spacing={2}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                }}
             >
-                <Grid container xs={12} md={6} lg={4} xl={3}
 
-                >
-                    
-                        <Box
-                            sx={{
-                                position: 'relative',
-                                p: { xs: 0, md: 0 },
-                                pr: { md: 0},
-                                left: 400,
-                                top:50,
-                            }}
-                        >
+                <Grid item xs={4} >
 
+                    <img src={props.logoUrl} width="45%" />
 
+                </Grid>
 
+            
+            <Grid item xs={4} >
 
-                            <img src={props.sponsorUrl} width="250%" />
+                <img src={props.sponsorUrl} width="160%" />
 
-
-
-                        </Box>+
-
-
-
-
-
-
-
-
-
-
-
-
-                    </Grid>
-                    
-                    <Box
-                            sx={{
-                                position: 'relative',
-                                p: { xs: 0, md: 1 },
-                                pr: { md: 0 },
-                                left: 1,
-                                bottom: 40,
-                            }}
-                        >
-                        <img src={props.logoUrl} width="20%" />
-                        </Box>
-                    
-
-                
-
-
-            </Box>
-        </Container>
+            </Grid>
+            </Grid>
+        </Container >
 
 
 
