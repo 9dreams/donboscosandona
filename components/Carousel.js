f62b0bd49eb8e18e853a938024a9563823ba982d
Filippo Urban <f.urban@donboscosandona.it>
2023-03-31 11:57:01 +0200

    fine carousel dimensioni

--- components/Carousel.js
+++ components/Carousel.js
@@ -18,7 +18,7 @@ export default function MyCarousel({ slides, maxWidth, height }) {
                 marginBottom: '2rem',
             }}
         >
-            <Carousel className={styles.carousel} animation="slide" sx={{height: height}}>
+            <Carousel className={styles.carousel} animation="slide" sx={{height: height + 'vh'}}>
                 {
                     slides.map(
                         (slide) => (
@@ -30,8 +30,8 @@ export default function MyCarousel({ slides, maxWidth, height }) {
                                     backgroundSize: 'cover',
                                     backgroundRepeat: 'no-repeat',
                                     backgroundPosition: 'center',
-                                    height: height,
-                                    paddingTop: '10'
+                                    height: height + 'vh',
+                                    paddingTop: (height/3) + 'vh'
                                 }}
                                 elevation={5}
                             >
@@ -77,5 +77,5 @@ export default function MyCarousel({ slides, maxWidth, height }) {
 
 MyCarousel.defaultProps = {
     maxWidth: false,
-    height: '90vh'
+    height: 90
 }
