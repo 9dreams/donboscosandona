fb88ed77a811547dd729635b68719c920517e9d5
dottorgross <a.ros@donboscosandona.it>
2023-04-06 10:41:35 +0200

    Pulizia del ramo main: viene lasciato solo il core dei componenti e una pagina per testarli

---
+++ pages/test_componenti.js
@@ -0,0 +1,227 @@
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+
+import Layout from '/components/Layout'
+import LandingHero from '/components/LandingHero2.js'
+import Features from '/components/Features'
+import Carousel from '/components/Carousel'
+import Products from '/components/Products'
+import Testimonials from '/components/Testimonials'
+import Team from '/components/Team'
+import Post from '/components/Post'
+import Certifications from '@/components/Certifications'
+import Maps from '/components/Maps'
+
+import { getDatiArticoli } from '/lib/articoli'
+
+// I punti di forza
+const features = [
+  {
+    title: "Progetto educativo",
+    imageUrl: "https://teamtrex.com.au/wp-content/uploads/2021/06/Strength-icon.png",
+    description: "Un progetto chiaro e affidabile per far crescere e diventare uomini. "
+  },
+  {
+    title: "Didattica attiva",
+    imageUrl: "https://teamtrex.com.au/wp-content/uploads/2021/06/Strength-icon.png",
+    description: "Lezioni attive e coinvolgenti per dimenticare la noia e partecipare da protagonisti."
+  },
+  {
+    title: "Laboratori",
+    imageUrl: "https://teamtrex.com.au/wp-content/uploads/2021/06/Strength-icon.png",
+    description: "12 ore settimanali in laboratori tecnologicamente all'avanguardia per formare i professionisti del futuro."
+  },
+  {
+    title: "Servizi al lavoro",
+    imageUrl: "https://teamtrex.com.au/wp-content/uploads/2021/06/Strength-icon.png",
+    description: "Orientamento, stage e alternanza, i nostri esperti ti accompagneranno fino all'effettivo inserimento nelle aziende del settore."
+  }
+]
+
+
+// slides per il carousel
+let slides = [
+  {
+    titolo: 'Fatti un bel giro',
+    descrizione: 'Scopri tutti i segreti del nostro Centro con il tour virtuale!',
+    immagine: 'https://source.unsplash.com/random',
+  },
+  {
+    titolo: 'Concorso nazionale settore elettrico',
+    descrizione: "A maggio il nostro Centro avrà l'onore di ospitare il Concorso Nazionale del Settore Elettrico: tutti i Centri di Formazione Professionale salesiani d'Italia invieranno i loro campioni per una settimana di sfida e condivisione professionale...",
+    colore: '#22aa22',
+  },
+]
+
+// Il nostro team
+let members = [
+  {
+    name: "don Nicola Munari",
+    role: "DIRETTORE / CATECHISTA",
+    description: "And I love you like Kanye",
+    description1: "loves Kanye. We need to restart the human foundation",
+    imageUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    email: "c.coso@donboscosandona.it",
+    phone: '0421 3388949874950',
+  },
+  {
+    name: "Alessandro Cappelletto",
+    role: "PRESIDE",
+    description: "And I love you like Kanye",
+    description1: "loves Kanye. We need to restart the human foundation",
+    imageUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    email: "c.ciaoooo@donboscosandona.it",
+    phone: '123456',
+  },
+  {
+    name: "Alessandro Ferro",
+    role: "VICE-PRESIDE / TUTOR DIGITALE",
+    description1: "Vicepreside, gestione organizzazione scolastica e pastorale, insegnamento area scientifica ",
+    imageUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1600",
+
+    email: "a.ferro@donboscosandona.it",
+    phone: '0421 111 222',
+  },
+  {
+    name: "Anna Maria D'Ambrosio",
+    role: "RESPONSABILE DELLA DISCIPLINA",
+    description1: "Gestione disciplina,organizzazione scolastica e insegnamento are cultura",
+    imageUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    email: "l.hdggfd@donboscosandona.it",
+    phone: '12365598445',
+  },
+]
+
+let prodotti = [
+  {
+    title: "Elettrico",
+    category: "QUALIFICA DI OPERATORE ELETTRICO",
+    description: "Installa ed effettua la manutenzione di impianti civili e industriali, sia con tecnologie tradizionali che automatizzate. Realizza la programmazione dei componenti domotici e dei controllori programmabili industriali.",
+    immagineUrl: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+    url: "/elettrico"
+  },
+  {
+    title: "Energia",
+    category: "QUALIFICA DI OPERATORE DI IMPIANTI TERMO-IDRAULICI",
+    description: "Interviene nell'installazione, collaudo e manutenzione di impianti termici, idraulici, di condizionamento e fotovoltaici, con una particolare attenzione al risparmio energetico, anche grazie alla building automation.",
+    immagineUrl: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+    url: "/energia"
+  },
+  {
+    title: "Informatico",
+    category: "DIPLOMA DI TECNICO INFORMATICO",
+    description: "Installa e configura hardware e software, esegue la manutenzione di sistemi, reti e terminali utente, fornisce assistenza tecnica, effettua l'elaborazione e la manutenzione di dati su archivi digitali. Con il quarto anno in Sistema Duale diventa tecnico sviluppatore di Soluzioni Software.",
+    immagineUrl: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+    url: "/informatico"
+  },
+]
+
+const testimonials = [
+  {
+    name: "Michele Venturato",
+    imageUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    text: "Grande scuola. Una vera scuola professionale e morale. Sforna ogni anno moltissimi tecnici aggiornati con I tempi e in grado di integrarsi con facilita' nel mondo del lavoro. Complimenti a tutto il sistema.",
+    social: "@Google"
+  },
+  {
+    imageUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    name: "Daniele Mladenovic",
+    text: "Ho trascorso alcuni anni presso il CFP e ho trovato l'ambiente molto accogliente e confortevole, grazie alla disponibilità di tutto il personale. In particolare, ho apprezzato la passione degli insegnanti, che si sono dimostrati molto competenti. Inoltre, ho avuto l'opportunità di partecipare ad attività extracurricolari e progetti che mi hanno permesso di sviluppare le mie capacità. Infine, consiglio questo istituto sottolineando l'importanza dello sviluppo e della scoperta dei propri talenti che è ciò che ci rende unici.",
+    social: "@BOBTEK"
+  },
+  {
+    imageUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    name: "Pino West",
+    text: "Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione ",
+    social: "@PINOWEST"
+  },
+]
+
+let certifications = [
+  {
+    logoUrl: 'https://seeklogo.com/images/C/CERMET_SINCERT-logo-129ABB30BE-seeklogo.com.png',
+    text1: 'ISO 9001:2015',
+    text2: 'Reg. n. 2593-A - Settore EA: 37'
+  },
+  {
+    logoUrl: 'https://seeklogo.com/images/C/CERMET_SINCERT-logo-129ABB30BE-seeklogo.com.png',
+    text1: 'Progettazione ed erogazione di attività formative e di orientamento'
+  },
+  {
+    logoUrl: 'https://seeklogo.com/images/C/CERMET_SINCERT-logo-129ABB30BE-seeklogo.com.png',
+    text1: 'Authorised Training Center'
+  },
+  {
+    logoUrl: 'https://seeklogo.com/images/C/CERMET_SINCERT-logo-129ABB30BE-seeklogo.com.png',
+    text1: 'Cod. A033 - Formazione Iniziale,',
+    text2: 'Superiore e Orientamento'
+  },
+]
+
+export default function Home({ datiArticoli }) {
+  return (
+    <Layout>
+      <LandingHero
+        opacity={0.5}
+        title="Centro di Formazione Professionale don Bosco"
+        description="Diventa adulto con il metodo educativo di don Bosco preparandoti ad entrare a testa alta nel mondo professionale di domani!"
+        buttonUrl='j'
+        buttonText="Guarda il video"
+        imageUrl="https://source.unsplash.com/random"
+      />
+      <Container maxWidth="lg" sx={{ marginTop: '3rem', marginBottom: '3rem' }}>
+        <Grid container spacing={4}>
+          {
+            datiArticoli.map(
+              (post) => (
+                <Post post={post} />
+              )
+            )
+          }
+        </Grid>
+      </Container>
+      <Carousel slides={slides} />
+      <Products
+        title="I prodotti"
+        description="Un'ampia scelta di indirizzi professionali: scopri quello che ti accende di più!"
+        cardWidth={4}
+        products={prodotti}
+      />
+      <Features
+        title="I nostri punti di forza"
+        description="Scopri perché il CFP DON BOSCO è davvero la scuola che fa al caso tuo!"
+        features={features}
+        cardWidth={3}
+      />
+      <Testimonials
+        testimonials={testimonials}
+        cardWidth={4}
+        imageUrl="https://images8.alphacoders.com/948/948649.jpg"
+      />
+      <Team
+        title="Contatti"
+        description="Formatori che ti aiutano a crescere con il cuore di don Bosco"
+        members={members}
+        cardWidth={3}
+      />
+      <Maps
+        maxWidth='100%'
+        maxHeight='550px'
+        url="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5580.05343317009!2d12.5710658!3d45.6301996!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477956fe076b4157%3A0x29fb231d47465883!2sCnos%20Fap%20Don%20Bosco!5e0!3m2!1sit!2sit!4v1680507660807!5m2!1sit!2sit"
+      />
+      <Certifications
+        cardWidth={3}
+        certifications={certifications}
+      />
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const datiArticoli = getDatiArticoli()
+  return {
+    props: {
+      datiArticoli,
+    },
+  }
+}
