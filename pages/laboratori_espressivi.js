095fd67d3e60f24b61f2b6d8abd76c5f1d037000
dottorgross <a.ros@donboscosandona.it>
2023-04-14 19:42:03 +0200

    Tolte righe inutili

--- pages/laboratori_espressivi.js
+++ pages/laboratori_espressivi.js
@@ -14,38 +14,12 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography'
 
 
-
-let prodotti = [
-  {
-    title: "Elettrico",
-    category: "QUALIFICA DI OPERATORE ELETTRICO",
-    description: "Installa ed effettua la manutenzione di impianti civili e industriali, sia con tecnologie tradizionali che automatizzate. Realizza la programmazione dei componenti domotici e dei controllori programmabili industriali.",
-    immagineUrl: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-    url: "/elettrico"
-  },
-  {
-    title: "Energia",
-    category: "QUALIFICA DI OPERATORE DI IMPIANTI TERMO-IDRAULICI",
-    description: "Interviene nell'installazione, collaudo e manutenzione di impianti termici, idraulici, di condizionamento e fotovoltaici, con una particolare attenzione al risparmio energetico, anche grazie alla building automation.",
-    immagineUrl: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-    url: "/energia"
-  },
-  {
-    title: "Informatico",
-    category: "DIPLOMA DI TECNICO INFORMATICO",
-    description: "Installa e configura hardware e software, esegue la manutenzione di sistemi, reti e terminali utente, fornisce assistenza tecnica, effettua l'elaborazione e la manutenzione di dati su archivi digitali. Con il quarto anno in Sistema Duale diventa tecnico sviluppatore di Soluzioni Software.",
-    immagineUrl: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-    url: "/informatico"
-  },
-]
-
 export default function Pagina() {
   return (
     <Layout>
       <LandingHero
         opacity={0.2}
         title="Laboratori espressivi"
-
         description="I laboratori espressivi sono spazi in cui le persone possono esplorare e comunicare le proprie emozioni e pensieri in modo creativo e spontaneo, attraverso l'utilizzo di diversi media espressivi, come la pittura, la musica, la danza, la scrittura e la fotografia."
         imageUrl="https://static01.nyt.com/images/2023/01/03/multimedia/03pinocchio-puppets-5-97e3/03pinocchio-puppets-5-97e3-superJumbo.jpg"
       />
