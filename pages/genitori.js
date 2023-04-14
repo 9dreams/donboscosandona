245768bd4241e3d1a60bb6a1ad3b83ca2fdc717e
Eros Simoncin <e.simoncin@donboscosandona.it>
2023-04-14 11:37:23 +0200

    fine

--- pages/genitori.js
+++ pages/genitori.js
@@ -12,16 +12,17 @@ export default function Pagina() {
         imageUrl="/images/genitori/genfem.png"
       />
       <Paragraph
-        title="Titolo"
+        title="Spazio genitori"
         subtitle="L’intento dell’Oratorio Don Bosco è di essere alleati dei genitori nell’educazione dei ragazzi, non sostituti!
         E visto che i nostri cortili non sono un parcheggio per i figli, è bello vedersi ogni tanto durante il periodo della P.E.R. per incontrarci, mettersi al servizio, scambiarci opinioni, ridere e scherzare…
         "
       >
         Ecco gli appuntamenti della P.E.R. dove i genitori sono stra-benvenuti:
--   	Domenica 18 giugno, ore 9: Santa Messa di Apertura; a seguire incontro per delle comunicazioni in teatro.
--   	Tutte le serate organizzate!
--   	l’Oratorio è sempre aperto… venite a rinfrescarvi da noi!
--   	Sabato 8 Luglio, ore 15.30: Santa Messa di Ringraziamento al Santuario di Motta di Livenza... e alla fine ANGURIA PER TUTTI!!!
+        <li>Domenica 18 giugno, ore 9: Santa Messa di Apertura; a seguire incontro per delle comunicazioni in teatro.</li>
+        <li>Tutte le serate organizzate!</li>
+        <li>L’Oratorio è sempre aperto… venite a rinfrescarvi da noi!</li>	
+        <li><strong>Sabato 8 Luglio, ore 15.30: Santa Messa di Ringraziamento al Santuario di Motta di Livenza... e alla fine ANGURIA PER TUTTI!!!</strong></li>
+
  
 Per coloro che volessero regalare un po’ del proprio tempo e dare una mano a Don Bosco durante la P.E.R., non esiti a contattare don Bogdan. A fine maggio una riunione organizzativa!!!
 
