979b18222c5c18a3150a0cbe65c44c105ab24dda
dottorgross <a.ros@donboscosandona.it>
2023-04-16 17:36:20 +0200

    Possibilità di inserire un'immagine di sfondo nei Paragraph

--- components/Paragraph.js
+++ components/Paragraph.js
@@ -1,88 +1,107 @@
-import { Container, Typography, CardMedia } from "@mui/material";
+import { Container, Typography, CardMedia, Box } from "@mui/material";
 
 export default function Paragraph(props) {
   return (
     <Container
       maxWidth={props.maxWidth}
       sx={{
+        position: "relative",
         marginTop: "2rem",
         padding: "3rem",
         textAlign: "justify",
         backgroundColor: props.backgroundColor,
         borderRadius: "1rem",
+        backgroundImage: "url(" + props.backgroundImageUrl + ")",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
         ...props.sx,
       }}
     >
-      {props.topImageUrl && (
-        <CardMedia
-          component="img"
-          image={props.topImageUrl}
-          width="100%"
-          sx={{
-            marginBottom: "3rem",
-          }}
-        />
-      )}
-      {props.title && (
-        <Typography variant="h4" component="h1" gutterBottom>
-          {props.title}
-        </Typography>
-      )}
-      {props.subtitle && (
-        <Typography variant="h5" component="h1" gutterBottom>
-          {props.subtitle}
-        </Typography>
-      )}
-      <Container
-        disableGutters={true}
+      <Box
         sx={{
-          marginTop: "2rem",
-          columnCount: { xs: 1, md: props.columnCount },
-          columnGap: "3rem",
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          backgroundColor: props.backgroundColor,
+          opacity: props.opacity,
+          borderRadius: "1rem",
         }}
-      >
-        <Typography variant="body1" gutterBottom>
-          {props.avatarImageUrl && (
-            <CardMedia
-              component="img"
-              image={props.avatarImageUrl}
-              sx={{
-                float: "left",
-                marginTop: "-1rem",
-                marginRight: "1rem",
-                width: "8rem",
-                borderRadius: "50%",
-              }}
-            />
-          )}
-          {props.leftImageUrl && (
-            <CardMedia
-              component="img"
-              image={props.leftImageUrl}
-              sx={{
-                float: "left",
-                marginRight: "1rem",
-                marginBottom: "1rem",
-                width: "20rem",
-                borderRadius: "5px",
-              }}
-            />
-          )}
-          {props.rightImageUrl && (
-            <CardMedia
-              component="img"
-              image={props.rightImageUrl}
-              sx={{
-                float: "right",
-                marginLeft: "1rem",
-                marginBottom: "1rem",
-                width: "20rem",
-                borderRadius: "5px",
-              }}
-            />
-          )}
-          {props.children}
-        </Typography>
+      />
+      <Container sx={{ position: "relative", color: props.color }}>
+        {props.topImageUrl && (
+          <CardMedia
+            component="img"
+            image={props.topImageUrl}
+            width="100%"
+            sx={{
+              marginBottom: "3rem",
+            }}
+          />
+        )}
+        {props.title && (
+          <Typography variant="h4" component="h1" gutterBottom>
+            {props.title}
+          </Typography>
+        )}
+        {props.subtitle && (
+          <Typography variant="h5" component="h1" gutterBottom>
+            {props.subtitle}
+          </Typography>
+        )}
+        <Container
+          disableGutters={true}
+          sx={{
+            marginTop: "2rem",
+            columnCount: { xs: 1, md: props.columnCount },
+            columnGap: "3rem",
+          }}
+        >
+          <Typography variant="body1" gutterBottom>
+            {props.avatarImageUrl && (
+              <CardMedia
+                component="img"
+                image={props.avatarImageUrl}
+                sx={{
+                  float: "left",
+                  marginTop: "-1rem",
+                  marginRight: "1rem",
+                  width: "8rem",
+                  borderRadius: "50%",
+                }}
+              />
+            )}
+            {props.leftImageUrl && (
+              <CardMedia
+                component="img"
+                image={props.leftImageUrl}
+                sx={{
+                  float: "left",
+                  marginRight: "1rem",
+                  marginBottom: "1rem",
+                  width: "20rem",
+                  borderRadius: "5px",
+                }}
+              />
+            )}
+            {props.rightImageUrl && (
+              <CardMedia
+                component="img"
+                image={props.rightImageUrl}
+                sx={{
+                  float: "right",
+                  marginLeft: "1rem",
+                  marginBottom: "1rem",
+                  width: "20rem",
+                  borderRadius: "5px",
+                }}
+              />
+            )}
+            {props.children}
+          </Typography>
+        </Container>
       </Container>
     </Container>
   );
@@ -92,4 +111,6 @@ Paragraph.defaultProps = {
   columnCount: 1,
   maxWidth: "lg",
   backgroundColor: false,
+  color: "#444",
+  opacity: 1,
 };
