add0cfc4902e2bbea722eb4429b974ea502905d0
dottorgross <a.ros@donboscosandona.it>
2023-04-26 13:03:05 +0200

    Calendario senza immagini

--- pages/calendario.js
+++ pages/calendario.js
@@ -3,36 +3,200 @@ import LandingHero from "/components/LandingHero2.js";
 import Paragraph from "components/Paragraph";
 import Calendar from "/components/Calendar.js";
 
-let settimana1 = [
-  {
-    title: "15 GIUGNO",
-    description: "Ini",
-    immagineUrl:
-      "http://www.inoratorio.it/img/column/f9de1e15784a9947c6263f665621c07723d13098.jpg?1651928152",
-  },
-];
-
 export default function Pagina() {
   return (
     <Layout>
       <LandingHero
-        opacity={0.2}
+        opacity={0.0}
         title="Calendario"
         imageUrl="/images/calendario.png"
       />
       <Calendar cardWidth={3} events={settimana0} />
+      <Calendar title="Prima settimana" cardWidth={3} events={settimana1} />
+      <Calendar title="Seconda settimana" cardWidth={3} events={settimana2} />
+      <Calendar title="Terza settimana" cardWidth={3} events={settimana3} />
+      <Calendar title="Quarta settimana" cardWidth={3} events={settimana4} />
     </Layout>
   );
 }
 
 let settimana0 = [
   {
-    date: "15 Giugno",
+    date: "Giovedì 15 Giugno",
     morning: "Inizio elementari",
     afternoon: "Inizio medie",
-    evening: "Serata film",
-    immagineUrl:
-      "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-    url: "/elettrico",
+    immagineUrl: "",
+  },
+  {
+    date: "Venerdì 16 Giugno",
+    morning: "Inizio attività",
+    afternoon: "Presentazione squadre e grande gioco",
+    immagineUrl: "",
+  },
+  {
+    date: "Domenica 18 Giugno",
+    morning: "Messa di apertura, incontro con i genitori, giochi e pranzo al sacco",
+    immagineUrl: "",
+  },
+];
+
+let settimana1 = [
+  {
+    date: "Lunedì 19 Giugno",
+    morning: "Attività regolare",
+    afternoon: "Giochi al parco e tornei",
+    evening: "Al Cinema",
+    immagineUrl: "",
+  },
+  {
+    date: "Martedì 20 Giugno",
+    morning: "Attività regolare",
+    afternoon: "Giochi al parco e tornei",
+    evening: "Oratorio's Got Talent",
+    immagineUrl: "",
+  },
+  {
+    date: "Mercoledì 21 Giugno",
+    morning: "Attività regolare",
+    afternoon: "Pit Stop animatori",
+    evening: "Tornei animatori",
+    immagineUrl: "",
+  },
+  {
+    date: "Giovedì 22 Giugno",
+    morning: "Gita a Noale",
+    afternoon: "Gita a Noale",
+    immagineUrl: "",
+  },
+  {
+    date: "Venerdì 23 Giugno",
+    morning: "Attività regolare",
+    afternoon: "Ospite DEKRU",
+    evening: "Spettacolo DEKRU",
+    immagineUrl: "",
+  },
+  {
+    date: "Sabato 24 Giugno",
+    morning: "Gita animatori",
+    immagineUrl: "",
+  },
+];
+
+let settimana2 = [
+  {
+    date: "Lunedì 26 Giugno",
+    morning: "Attività regolare",
+    afternoon: "Quinta elementare e prima media al mare",
+    evening: "Al Cinema",
+    immagineUrl: "",
+  },
+  {
+    date: "Martedì 27 Giugno",
+    morning: "Gita in montagna: VALLE DEL GARES",
+    afternoon: "Gita in montagna: VALLE DEL GARES",
+    immagineUrl: "",
+  },
+  {
+    date: "Mercoledì 28 Giugno",
+    morning: "Attività regolare",
+    afternoon: "Pit Stop animatori",
+    evening: "Tornei animatori",
+    immagineUrl: "",
+  },
+  {
+    date: "Giovedì 29 Giugno",
+    morning: "Attività regolare",
+    afternoon: "Giochi al parco e tornei",
+    immagineUrl: "",
+  },
+  {
+    date: "Venerdì 30 Giugno",
+    morning: "Attività regolare",
+    afternoon: "Seconda e terza media al mare",
+    evening: "Spettacolo DUNK ITALY SHOW",
+    immagineUrl: "",
+  },
+];
+
+let settimana3 = [
+  {
+    date: "Lunedì 3 Luglio",
+    morning: "Cambio attività",
+    afternoon: "Terza e quarta elementare al mare",
+    evening: "Al Cinema",
+    immagineUrl: "",
+  },
+  {
+    date: "Martedì 4 Luglio",
+    morning: "Gita al PARCO SIGURTA'",
+    afternoon: "Gita al PARCO SIGURTA'",
+    immagineUrl: "",
+  },
+  {
+    date: "Mercoledì 5 Luglio",
+    morning: "Attività regolare",
+    afternoon: "Pit Stop animatori",
+    evening: "Tornei animatori",
+    immagineUrl: "",
+  },
+  {
+    date: "Giovedì 6 Luglio",
+    morning: "Attività regolare",
+    afternoon: "Giochi al parco e tornei",
+    evening: "Sfilata a squadre e giochi a stand con i genitori",
+    immagineUrl: "",
+  },
+  {
+    date: "Venerdì 7 Luglio",
+    morning: "Attività regolare",
+    afternoon: "Giochi al parco e tornei",
+    evening: "Pellegrinaggio notturno terze medie",
+    immagineUrl: "",
+  },
+  {
+    date: "Sabato 8 Luglio",
+    morning: "Pellegrinaggio",
+    immagineUrl: "",
+  },
+];
+
+let settimana4 = [
+  {
+    date: "Lunedì 10 Luglio",
+    morning: "Attività regolare",
+    afternoon: "Quinta elementare e prima media al mare",
+    evening: "Al Cinema",
+    immagineUrl: "",
+  },
+  {
+    date: "Martedì 11 Luglio",
+    morning: "Gita a Noale",
+    afternoon: "Gita a Noale",
+    immagineUrl: "",
+  },
+  {
+    date: "Mercoledì 12 Luglio",
+    morning: "Attività regolare",
+    afternoon: "Pit Stop animatori",
+    evening: "Tornei animatori",
+    immagineUrl: "",
+  },
+  {
+    date: "Giovedì 13 Luglio",
+    morning: "Gita terze medie",
+    afternoon: "Giochi al parco e tornei",
+    immagineUrl: "",
+  },
+  {
+    date: "Venerdì 14 Luglio",
+    morning: "Attività regolare",
+    afternoon: "Giochi al parco e tornei",
+    evening: "SERATA FINALE",
+    immagineUrl: "",
+  },
+  {
+    date: "Lunedì 17 Luglio",
+    evening: "Al Cinema",
+    immagineUrl: "",
   },
 ];
