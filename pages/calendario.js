cca66f0b323e7b64a2cace0adbb59b79f3306bfa
dottorgross <a.ros@donboscosandona.it>
2023-04-26 16:54:55 +0200

    Calendario

--- pages/calendario.js
+++ pages/calendario.js
@@ -11,7 +11,7 @@ export default function Pagina() {
         title="Calendario"
         imageUrl="/images/calendario.png"
       />
-      <Calendar cardWidth={3} events={settimana0} />
+      <Calendar cardWidth={4} events={settimana0} />
       <Calendar title="Prima settimana" cardWidth={3} events={settimana1} />
       <Calendar title="Seconda settimana" cardWidth={3} events={settimana2} />
       <Calendar title="Terza settimana" cardWidth={3} events={settimana3} />
@@ -25,18 +25,18 @@ let settimana0 = [
     date: "Giovedì 15 Giugno",
     morning: "Inizio elementari",
     afternoon: "Inizio medie",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/inizio.png",
   },
   {
     date: "Venerdì 16 Giugno",
     morning: "Inizio attività",
     afternoon: "Presentazione squadre e grande gioco",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/inizio2.png",
   },
   {
     date: "Domenica 18 Giugno",
     morning: "Messa di apertura, incontro con i genitori, giochi e pranzo al sacco",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/messa.png",
   },
 ];
 
@@ -45,40 +45,40 @@ let settimana1 = [
     date: "Lunedì 19 Giugno",
     morning: "Attività regolare",
     afternoon: "Giochi al parco e tornei",
-    evening: "Al Cinema",
-    immagineUrl: "",
+    evening: "ore 20.45 Film TRANSFORMERS - IL RISVEGLIO",
+    immagineUrl: "/images/calendario/transformers.png",
   },
   {
     date: "Martedì 20 Giugno",
     morning: "Attività regolare",
     afternoon: "Giochi al parco e tornei",
     evening: "Oratorio's Got Talent",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/talent.png",
   },
   {
     date: "Mercoledì 21 Giugno",
     morning: "Attività regolare",
     afternoon: "Pit Stop animatori",
     evening: "Tornei animatori",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/animatori.png",
   },
   {
     date: "Giovedì 22 Giugno",
     morning: "Gita a Noale",
     afternoon: "Gita a Noale",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/noale.png",
   },
   {
     date: "Venerdì 23 Giugno",
     morning: "Attività regolare",
     afternoon: "Ospite DEKRU",
     evening: "Spettacolo DEKRU",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/dekru.png",
   },
   {
     date: "Sabato 24 Giugno",
     morning: "Gita animatori",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/gita_animatori.png",
   },
 ];
 
@@ -87,34 +87,34 @@ let settimana2 = [
     date: "Lunedì 26 Giugno",
     morning: "Attività regolare",
     afternoon: "Quinta elementare e prima media al mare",
-    evening: "Al Cinema",
-    immagineUrl: "",
+    evening: "ore 20.45 Film ELEMENTAL 3D",
+    immagineUrl: "/images/calendario/elemental.png",
   },
   {
     date: "Martedì 27 Giugno",
     morning: "Gita in montagna: VALLE DEL GARES",
     afternoon: "Gita in montagna: VALLE DEL GARES",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/gares.png",
   },
   {
     date: "Mercoledì 28 Giugno",
     morning: "Attività regolare",
     afternoon: "Pit Stop animatori",
     evening: "Tornei animatori",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/animatori.png",
   },
   {
     date: "Giovedì 29 Giugno",
     morning: "Attività regolare",
     afternoon: "Giochi al parco e tornei",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/regolare.png",
   },
   {
     date: "Venerdì 30 Giugno",
     morning: "Attività regolare",
     afternoon: "Seconda e terza media al mare",
     evening: "Spettacolo DUNK ITALY SHOW",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/dunk.png",
   },
 ];
 
@@ -123,40 +123,40 @@ let settimana3 = [
     date: "Lunedì 3 Luglio",
     morning: "Cambio attività",
     afternoon: "Terza e quarta elementare al mare",
-    evening: "Al Cinema",
-    immagineUrl: "",
+    evening: "ore 20.45 Film DUNGEONS & DRAGONS",
+    immagineUrl: "/images/calendario/dungeons.png",
   },
   {
     date: "Martedì 4 Luglio",
     morning: "Gita al PARCO SIGURTA'",
     afternoon: "Gita al PARCO SIGURTA'",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/sigurtà.png",
   },
   {
     date: "Mercoledì 5 Luglio",
     morning: "Attività regolare",
     afternoon: "Pit Stop animatori",
     evening: "Tornei animatori",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/animatori.png",
   },
   {
     date: "Giovedì 6 Luglio",
     morning: "Attività regolare",
     afternoon: "Giochi al parco e tornei",
     evening: "Sfilata a squadre e giochi a stand con i genitori",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/sfilata.png",
   },
   {
     date: "Venerdì 7 Luglio",
     morning: "Attività regolare",
     afternoon: "Giochi al parco e tornei",
     evening: "Pellegrinaggio notturno terze medie",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/regolare.png",
   },
   {
     date: "Sabato 8 Luglio",
     morning: "Pellegrinaggio",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/pellegrinaggio.png",
   },
 ];
 
@@ -165,38 +165,38 @@ let settimana4 = [
     date: "Lunedì 10 Luglio",
     morning: "Attività regolare",
     afternoon: "Quinta elementare e prima media al mare",
-    evening: "Al Cinema",
-    immagineUrl: "",
+    evening: "ore 20.45 Film INDIANA JONES E IL QUADRANTE DEL DESTINO 3D",
+    immagineUrl: "/images/calendario/indiana.png",
   },
   {
     date: "Martedì 11 Luglio",
     morning: "Gita a Noale",
     afternoon: "Gita a Noale",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/noale.png",
   },
   {
     date: "Mercoledì 12 Luglio",
     morning: "Attività regolare",
     afternoon: "Pit Stop animatori",
     evening: "Tornei animatori",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/animatori.png",
   },
   {
     date: "Giovedì 13 Luglio",
     morning: "Gita terze medie",
     afternoon: "Giochi al parco e tornei",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/regolare.png",
   },
   {
     date: "Venerdì 14 Luglio",
     morning: "Attività regolare",
     afternoon: "Giochi al parco e tornei",
     evening: "SERATA FINALE",
-    immagineUrl: "",
+    immagineUrl: "/images/calendario/finale.png",
   },
   {
     date: "Lunedì 17 Luglio",
-    evening: "Al Cinema",
-    immagineUrl: "",
+    evening: "ore 20.45 Film MISSION IMPOSSIBLE - DEAD RECKONING",
+    immagineUrl: "/images/calendario/mission.png",
   },
 ];
