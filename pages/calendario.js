1440441cd676eeaf45ce29519b333e03aa3f3ad3
dottorgross <a.ros@donboscosandona.it>
2023-04-26 12:33:13 +0200

    Calendario

--- pages/calendario.js
+++ pages/calendario.js
@@ -1,15 +1,16 @@
-import Layout from '/components/Layout'
-import LandingHero from '/components/LandingHero2.js'
-import Paragraph from 'components/Paragraph'
-import Products from '/components/Products.js'
+import Layout from "/components/Layout";
+import LandingHero from "/components/LandingHero2.js";
+import Paragraph from "components/Paragraph";
+import Calendar from "/components/Calendar.js";
 
 let settimana1 = [
   {
     title: "15 GIUGNO",
     description: "Ini",
-    immagineUrl: "http://www.inoratorio.it/img/column/f9de1e15784a9947c6263f665621c07723d13098.jpg?1651928152",
+    immagineUrl:
+      "http://www.inoratorio.it/img/column/f9de1e15784a9947c6263f665621c07723d13098.jpg?1651928152",
   },
-]
+];
 
 export default function Pagina() {
   return (
@@ -19,10 +20,19 @@ export default function Pagina() {
         title="Calendario"
         imageUrl="/images/calendario.png"
       />
-      <Products
-        products={settimana1}
-        cardWidth={4}
-      />
+      <Calendar cardWidth={3} events={settimana0} />
     </Layout>
-  )
+  );
 }
+
+let settimana0 = [
+  {
+    date: "15 Giugno",
+    morning: "Inizio elementari",
+    afternoon: "Inizio medie",
+    evening: "Serata film",
+    immagineUrl:
+      "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+    url: "/elettrico",
+  },
+];
