5db83812bfd1d0cc4ee2252e61c32450c2b97024
dottorgross <a.ros@donboscosandona.it>
2023-05-05 17:58:57 +0200

    Indice componenti

---
+++ components/index.js
@@ -0,0 +1,21 @@
+export {default as Calendar } from './Calendar'
+export {default as Carousel } from './Carousel'
+export {default as Certifications } from './Certifications'
+export {default as Date } from './Date'
+export {default as Features } from './Features'
+export {default as Footer } from './Footer'
+export {default as Header } from './Header'
+export {default as LandingHero } from './LandingHero2'
+export {default as Layout } from './Layout'
+export {default as Logos } from './Logos'
+export {default as Maps } from './Maps'
+export {default as Paragraph } from './Paragraph'
+export {default as Post } from './Post'
+export {default as Products } from './Products'
+export {default as Table } from './Table'
+export {default as Team } from './Team'
+export {default as Testimonials } from './Testimonials'
+
+// Reference:
+// https://sunnysingh.io/blog/javascript-import-from-folder
+// Spiega anche come esportare più componenti (named e default) dallo stesso file
