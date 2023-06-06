63cbe13c770c9550f946242a47d57e8abf43890d
dottorgross <a.ros@donboscosandona.it>
2023-06-06 16:14:00 +0200

    Modifiche al componente Date

--- components/Date.js
+++ components/Date.js
@@ -1,9 +1,10 @@
-import { parseISO, format } from 'date-fns';
+import { parseISO } from 'date-fns';
 
-export default function Date({ dateString }) {
+export default function Date2({ dateString }) {
+  var options = {'weekday': 'long', 'month': 'long', 'day': 'numeric', 'year': 'numeric'};
   if (dateString) {
-    const date = parseISO(dateString)
-    return <time dateTime={dateString}>{format(date, 'd MMMM yyyy')}</time>
+    const date = parseISO(dateString).toLocaleString('it-IT', options);
+    return <div>{date}</div>
   }
   else {
     return ''
