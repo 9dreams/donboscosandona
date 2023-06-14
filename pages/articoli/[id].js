02f822b1e93171d954cbe3ea8d437d835e56a607
dottorgross <a.ros@donboscosandona.it>
2023-06-14 17:24:23 +0200

    Metadati articoli

--- pages/articoli/[id].js
+++ pages/articoli/[id].js
@@ -9,48 +9,56 @@ import LandingHero from '/components/LandingHero2'
 import { getIdArticoli, getDatiArticolo } from '/lib/articoli'
 
 export default function Articolo({ datiArticolo }) {
-    return (
-        <Layout>
-            <Head>
-                <title>{datiArticolo.title}</title>
-                <meta name="og:title" content={datiArticolo.title} />
-            </Head>
-            <LandingHero
-                opacity={0.5}
-                title={datiArticolo.title}
-                description={datiArticolo.abstract}
-                imageUrl={datiArticolo.imageUrl}
-            />
-            <Container
-                maxWidth="lg"
-                sx={{
-                    paddingTop: '5rem',
-                    paddingBottom: '5rem',
-                    minHeight: '70vh'
-                }}
-            >
-                <Date dateString={datiArticolo.date} />
-                <div dangerouslySetInnerHTML={{ __html: datiArticolo.contentHtml }} />
-            </Container>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <Head>
+        <title>{datiArticolo.title}</title>
+        <meta name='og:url' content={'https://per.donboscosandona.it/articoli/' + datiArticolo.id } />
+        <meta name='og:type' content='website' />
+        <meta name='og:locale' content='it_IT' />
+        <meta
+          name='og:title'
+          content={datiArticolo.title}
+        />
+        <meta name='og:description' content={datiArticolo.abstract} />
+        <meta name='og:image' content={datiArticolo.imageUrl} />
+      </Head>
+      <LandingHero
+        opacity={0.5}
+        title={datiArticolo.title}
+        description={datiArticolo.abstract}
+        imageUrl={datiArticolo.imageUrl}
+      />
+      <Container
+        maxWidth='lg'
+        sx={{
+          paddingTop: '5rem',
+          paddingBottom: '5rem',
+          minHeight: '70vh',
+        }}
+      >
+        <Date dateString={datiArticolo.date} />
+        <div dangerouslySetInnerHTML={{ __html: datiArticolo.contentHtml }} />
+      </Container>
+    </Layout>
+  )
 }
 
 export async function getStaticPaths() {
-    const paths = getIdArticoli()
+  const paths = getIdArticoli()
 
-    return {
-        paths,
-        fallback: false,
-    }
+  return {
+    paths,
+    fallback: false,
+  }
 }
 
 export async function getStaticProps({ params }) {
-    const datiArticolo = await getDatiArticolo(params.id)
+  const datiArticolo = await getDatiArticolo(params.id)
 
-    return {
-        props: {
-            datiArticolo,
-        },
-    }
+  return {
+    props: {
+      datiArticolo,
+    },
+  }
 }
