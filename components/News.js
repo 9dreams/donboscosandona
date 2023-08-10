b43e1d1cc422d0d6017fe02b4ec1ec88822e4508
dottorgross <a.ros@donboscosandona.it>
2023-08-10 22:02:58 +0200

    getServerSideProps anche nella pagina

--- components/News.js
+++ components/News.js
@@ -1,15 +1,8 @@
-import useSWR from 'swr'
-
 import { Container, Grid, Typography } from '@mui/material'
 
 import NewsCard from '/components/NewsCard'
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json())
-
-export default function News({ title, channel, limit }) {
-  const { data, error } = useSWR('https://channels.donboscosandona.it/api/posts/' + channel, fetcher)
-
-  if (error || data && data.status=='404') return <div>Errore di caricamento delle news.</div>
+export default function News({ title, data, limit }) {
   if (!data) return <div>Caricamento...</div>
   
   data.splice(limit)
