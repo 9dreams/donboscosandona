ef65ebb9139fe321a2655908008e0aca1b28a254
dottorgross <a.ros@donboscosandona.it>
2023-08-18 16:33:20 +0200

    Sostituito LandingHero2

--- pages/rassegna_stampa.js
+++ pages/rassegna_stampa.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Layout from '/components/Layout'
-import LandingHero from '/components/LandingHero2'
+import LandingHero from '/components/LandingHero'
 import Products from '/components/Products'
 
 let rassegna_stampa = [
