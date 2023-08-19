18b9455ba027d8cc5f54a0ca01fd1055a6af2cd0
dottorgross <a.ros@donboscosandona.it>
2023-08-19 16:04:05 +0200

    CardMedia visibile anche su dispositivi xs

--- components/NewsCard.js
+++ components/NewsCard.js
@@ -8,8 +8,22 @@ import {
 
 export default function NewsCard({ post }) {
   return (
-    <CardActionArea component='a' href={post.articolo && '/articoli/show?id=' + post.id || post.link || post.allegato}>
-      <Card sx={{ display: 'flex' }}>
+    <CardActionArea
+      component='a'
+      href={
+        (post.articolo && '/articoli/show?id=' + post.id) ||
+        post.link ||
+        post.allegato
+      }
+      disabled={!post.articolo && !post.link && !post.allegato}
+    >
+      <Card sx={{ display: {xs: 'block', sm: 'flex'} }}>
+        <CardMedia
+          component='img'
+          sx={{ width: '100%', display: { xs: 'block', sm: 'none' } }}
+          image={post.immagine}
+          alt={post.titolo}
+        />
         <CardContent sx={{ flex: 1 }}>
           <Typography component='h2' variant='h5'>
             {post.titolo}
@@ -20,16 +34,16 @@ export default function NewsCard({ post }) {
           <Typography variant='subtitle1' paragraph>
             {post.abstract}
           </Typography>
-          {post.articolo &&
-          <Typography variant='subtitle1' color='primary'>
-            Continua a leggere...
-          </Typography>
-          }
-          {!post.articolo && post.allegato &&
-          <Typography variant='subtitle1' color='primary'>
-            Scarica l'allegato
-          </Typography>
-          }
+          {post.articolo && (
+            <Typography variant='subtitle1' color='primary'>
+              Continua a leggere...
+            </Typography>
+          )}
+          {!post.articolo && post.allegato && (
+            <Typography variant='subtitle1' color='primary'>
+              Scarica l'allegato
+            </Typography>
+          )}
         </CardContent>
         <CardMedia
           component='img'
