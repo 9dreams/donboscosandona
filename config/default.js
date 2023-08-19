c87cfe7856ecfc6c8790acdfc821135df801b2c1
dottorgross <a.ros@donboscosandona.it>
2023-08-19 18:01:31 +0200

    Footer

--- config/default.js
+++ config/default.js
@@ -18,12 +18,7 @@ export const logoUrl =
 
 export const header = <NavBar menu={menu} siteName={siteName} logoUrl={logoUrl} />
 
-const menuFooter = [
-    { title: 'Informatico', url: '/informatico' },
-    { title: 'Energie', url: '/energie' },
-    { title: 'Elettrico', url: '/elettrico' },
-    { title: 'Trasparenza', url: '/trasparenza' },
-]
+const menuFooter = menu
 
 const socials = [
     { title: 'Facebook', imageUrl: 'https://www.donboscosandona.it/img/ck/1cffc9d197e15de1f72a89477cc75e56073980b0.png', url: 'https://www.facebook.com/donboscosandona/?fref=ts' },
@@ -33,27 +28,70 @@ const socials = [
 ]
 
 let images = [
-    { title: '', imageUrl: 'https://www.donboscosandona.it/img/column/f65e4622afa225a8f6f407cff0b9e616279eea44.jpg?1570719707', },
-    { title: '', imageUrl: 'https://www.donboscosandona.it/img/column/f65e4622afa225a8f6f407cff0b9e616279eea44.jpg?1570719707', },
-    { title: '', imageUrl: 'https://www.donboscosandona.it/img/column/f65e4622afa225a8f6f407cff0b9e616279eea44.jpg?1570719707', },
-    { title: '', imageUrl: 'https://www.donboscosandona.it/img/column/f65e4622afa225a8f6f407cff0b9e616279eea44.jpg?1570719707', },
-    { title: '', imageUrl: 'https://www.donboscosandona.it/img/column/f65e4622afa225a8f6f407cff0b9e616279eea44.jpg?1570719707', },
-    { title: '', imageUrl: 'https://www.donboscosandona.it/img/column/f65e4622afa225a8f6f407cff0b9e616279eea44.jpg?1570719707', },
+    { imageUrl: 'https://archive.donboscosandona.it/img/column/42480c58c90ada8ff75c443fdde83ae71fea75ac.jpg?1539529685', },
+    { imageUrl: 'https://archive.donboscosandona.it/img/column/417afaa0f0697f0f0aabf9ccf61030769cb76f14.jpg?1539529180', },
+    { imageUrl: 'https://archive.donboscosandona.it/img/column/97f264e9d16a0856dd2cb973630af1cac4426f73.jpg?1539529314', },
+    { imageUrl: 'https://archive.donboscosandona.it/img/column/b0bd034769ac3471bf375239f1e64e39f5684aa6.jpg?1539523300', },
+    { imageUrl: 'https://archive.donboscosandona.it/img/column/342f349c6bb5bbf9a37b95353df2bee2309b9792.jpg?1539524741', },
 ]
 
 export const footer = <Footer
     imageUrl="https://immagine di sfondo"
-    color="#272727"
+    color="#353b48"
     opacitycolor="1"
     opacity={1}
-    title1="About Us"
-    description1="Footer che apparirà sul fondo di tutte le pagine del sito. Sarà possibile passare al componente un'immagine di sfondo opzionale oppure un colore di sfondo. L'attributo opaciy permetterà di sovrapporre all'immagine un Box con il colore di sfondo e un'opacità regolabile.
-La prima colonna sarà destinata a un testo descrittivo, la seconda ai social, la terza a un menù di navigazione grafico (che porta ad es. ai settori).
-"
-    title2="Social Feed"
+    title1="Chi siamo"
+    description1="La SFP “don Bosco” di San Donà di Piave (VE) è una scuola della formazione professionale salesiana che prepara i giovani al mondo del lavoro, accompagnandoli in un percorso integrale di crescita umana e professionale."
+    title2="Social"
     socials={socials}
-    title3="I Settori"
+    title3=""
     images={images}
     menu={menuFooter}
-    copyright="Copyright (C) 2023 9dreams Agency."
+    copyright="Site by 9dreams Agency"
 />
+
+
+
+let settori = [
+    {
+      title: 'Elettrico',
+      category: 'QUALIFICA DI OPERATORE ELETTRICO',
+      description:
+        'Installa ed effettua la manutenzione di impianti civili e industriali, sia con tecnologie tradizionali che automatizzate. Realizza la programmazione dei componenti domotici e dei controllori programmabili industriali.',
+      immagineUrl:
+        'https://archive.donboscosandona.it/img/column/42480c58c90ada8ff75c443fdde83ae71fea75ac.jpg?1539529685',
+    },
+    {
+      title: 'Energia',
+      category: 'QUALIFICA DI OPERATORE DI IMPIANTI TERMO-IDRAULICI',
+      description:
+        "Interviene nell'installazione, collaudo e manutenzione di impianti termici, idraulici, di condizionamento e fotovoltaici, con una particolare attenzione al risparmio energetico, anche grazie alla building automation.",
+      immagineUrl:
+        'https://archive.donboscosandona.it/img/column/417afaa0f0697f0f0aabf9ccf61030769cb76f14.jpg?1539529180',
+    },
+    {
+      title: 'Informatico',
+      category: 'DIPLOMA DI TECNICO INFORMATICO',
+      description:
+        "Installa e configura hardware e software, esegue la manutenzione di sistemi, reti e terminali utente, fornisce assistenza tecnica, effettua l'elaborazione e la manutenzione di dati su archivi digitali. Con il quarto anno in Sistema Duale diventa tecnico sviluppatore di Soluzioni Software.",
+      immagineUrl:
+        'https://archive.donboscosandona.it/img/column/97f264e9d16a0856dd2cb973630af1cac4426f73.jpg?1539529314',
+    },
+    {
+      title: 'Meccanico',
+      category:
+        'DIPLOMA DI TECNICO PER LA CONDUZIONE E LA MANUTENZIONE DI IMPIANTI AUTOMATIZZATI',
+      description:
+        'Realizza lavorazioni al tornio, fresatrice e macchine utensili in generale. Partendo dal progetto realizzato con il CAD effettua la programmazione delle macchine a controllo numerico. Realizza saldature ad arco, TIG, MAG, taglio al plasma.',
+      immagineUrl:
+        'https://archive.donboscosandona.it/img/column/b0bd034769ac3471bf375239f1e64e39f5684aa6.jpg?1539523300',
+    },
+    {
+      title: 'Motoristico / Carrozzeria',
+      category: 'QUALIFICA DI OPERATORE ALLA RIPARAZIONE DI VEICOLI A MOTORE',
+      description:
+        "Effettua riparazioni e manutenzione dei motori e dell'autoveicolo in tutte le sue parti. Effettua diagnosi e prove sui gas di scarico intervenendo per risolvere le anomalie.",
+      immagineUrl:
+        'https://archive.donboscosandona.it/img/column/342f349c6bb5bbf9a37b95353df2bee2309b9792.jpg?1539524741',
+    },
+  ]
