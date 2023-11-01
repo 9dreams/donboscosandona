4a4a771030a9b358e17487a95db9f00149f831be
Filippo Bagnoli <f.bagnoli@donboscosandona.it>
2023-11-01 23:43:11 +0100

    wiew telefono e relayout

--- components/Torneo.js
+++ components/Torneo.js
@@ -1,28 +1,11 @@
 import React, { useState } from "react";
 import Styles from "./Torneo.module.css";
-import { Container, Grid,Typography } from "@mui/material";
+import { Container, Grid, Typography, Button } from "@mui/material";
 
 const Torneo = () => {
   const [rotatePizza, setRotatePizza] = useState(false);
   const [activePizza, setActivePizza] = useState(false);
-
   const [activePizza1, setActivePizza1] = useState(false);
-
-  const toggleRotatePizza = () => {
-    if (activePizza) {
-      setActivePizza(false);
-      setRotatePizza(false);
-      setActivePizza1(false)
-    } else {
-      setRotatePizza(true);
-      setTimeout(() => {
-        setActivePizza(true);
-      }, 1250);
-      setTimeout(() => {
-        setActivePizza1(true)
-      }, 2450);
-    }
-  };
 
   const numPizzettes = 15;
   const numPizzettes1 = 15;
@@ -55,64 +38,89 @@ const Torneo = () => {
     />
   ));
 
+  const toggleRotatePizza = () => {
+    if (activePizza) {
+      setActivePizza(false);
+      setRotatePizza(false);
+      setActivePizza1(false);
+    } else {
+      setRotatePizza(true);
+      setTimeout(() => {
+        setActivePizza(true);
+      }, 1250);
+      setTimeout(() => {
+        setActivePizza1(true);
+      }, 2450);
+    }
+  };
+
   return (
     <Container
       maxWidth
-      disableGutters={true}
       style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
         height: "80vh",
         backgroundColor: "#588fe8",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
         position: "relative",
         overflow: "hidden",
+        display: "flex",
       }}
     >
-      <div>
-        <img
-          className={`${Styles.rotatingImages} ${
-            rotatePizza ? Styles.pizzaPodio : ""
-          }`}
-          src={
-            activePizza
-              ? "/images/pizza/pizzadietro1.png"
-              : "/images/pizza/pizza.png"
-          }
-          alt="Pizza"
-          style={{ zIndex: "1" }}
-        />
-        {activePizza1 && (
-        <div
-          className={` ${
-            activePizza1 ? Styles.punteggio : ""
-          }`}
-        ><Grid item xs={6} sx={{textAlign:'center'}}><h3>Podio</h3></Grid>
-          <Grid container>
-          <Grid item xs={3} ></Grid>
+      <Grid container>
+        <Grid item xs={12} sm={4}>
+          <Typography>Colonna 1</Typography>
+        </Grid>
 
-            <Grid item xs={3} sx={{paddingTop: '30px',}}>
-              <Typography variant="body1">2F</Typography>
-            </Grid>
+        <Grid item xs={12} sm={4}>
+            <img
+              className={`${Styles.rotatingImages} ${
+                rotatePizza ? Styles.pizzaPodio : ""
+              }`}
+              src={
+                activePizza
+                  ? "/images/pizza/pizzadietro1.png"
+                  : "/images/pizza/pizza.png"
+              }
+              alt="Pizza"
+              style={{ zIndex: "1" }}
+            />
+            {activePizza1 && (
+              <div className={` ${activePizza1 ? Styles.punteggio : ""}`}>
+                <Grid item xs={12} sx={{top:20, position: 'absolute', left: 160}}>
+                  <Typography variant="h5" sx={{ }}>
+                    Podio
+                  </Typography>
+                </Grid>
+                <Grid container sx={{top:90, position: 'absolute', left: 60,zIndex: 5}}>
+                  <Grid item xs={1}></Grid>
+                  <Grid item xs={3} >
+                    <Typography variant="body1" sx={{paddingTop: 2, fontSize: '18px'}}>2F</Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Typography variant="body1" sx={{color: 'black', fontSize: '18px'}}>2D</Typography>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Typography variant="body1" sx={{paddingTop: 3, fontSize: '18px'}}>3F</Typography>
+                  </Grid>
+                </Grid>
+              </div>
+            )}
+          
 
-            <Grid item  xs={3} sx={{paddingTop: '10px'}}>
-              <Typography variant="body1">3F</Typography>
-            </Grid>
+          <div>
+            <Button
+              className={Styles.button}
+              style={{ marginTop: "20px" }}
+              onClick={toggleRotatePizza}
+            >
+              Gira Pizza
+            </Button>
+          </div>
+        </Grid>
 
-            <Grid item sx={{paddingTop: '40px'}}>
-              <Typography variant="body1">2D</Typography>
-            </Grid>
-            
-          </Grid>
-        </div>
-        )}
-      </div>
-
-      <div>
-        <button onClick={toggleRotatePizza}>Gira Pizza</button>
-      </div>
+        <Grid item xs={12} sm={4}>
+          <Typography>Colonna 3</Typography>
+        </Grid>
+      </Grid>
 
       <div>{pizzettes}</div>
       <div>{pizzettes1}</div>
