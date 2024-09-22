aed55f2160190b6b14ed3da90fb0c9c5e32e2d67
dottorgross <77401433+dottorgross@users.noreply.github.com>
2024-09-22 12:20:57 +0200

    Strumenti

--- pages/index.js
+++ pages/index.js
@@ -60,18 +60,28 @@ export default function Home({ data, elementi }) {
         prendere decisioni importanti quali il percorso scolastico,
         universitario o professionale.
       </Paragraph>
+
       <SwiperNews
         title='Articoli recenti'
         data={data}
         limit={6}
         defaultTag=''
       />
+
       <Products
         title='Servizi offerti'
         description=''
         cardWidth={4}
         cardWidthXs={12}
         products={servizi}
+      />
+      <Products
+        title='Strumenti usati'
+        description=''
+        products={strumenti}
+        cardWidth={3}
+        borderRadius='50%'
+        aspectRatio='1 / 1'
       />
 
       <Maps
@@ -103,128 +113,57 @@ export async function getStaticProps() {
   }
 }
 
-// I punti di forza
-const features = [
-  {
-    title: 'Progetto educativo',
-    imageUrl:
-      'https://teamtrex.com.au/wp-content/uploads/2021/06/Strength-icon.png',
-    description:
-      'Un progetto chiaro e affidabile per far crescere e diventare uomini. ',
-    url: 'https://cinema.donboscosandona.it',
-  },
-  {
-    title: 'Didattica attiva',
-    imageUrl:
-      'https://teamtrex.com.au/wp-content/uploads/2021/06/Strength-icon.png',
-    description:
-      'Lezioni attive e coinvolgenti per dimenticare la noia e partecipare da protagonisti.',
-  },
-  {
-    title: 'Laboratori',
-    imageUrl:
-      'https://teamtrex.com.au/wp-content/uploads/2021/06/Strength-icon.png',
-    description:
-      "12 ore settimanali in laboratori tecnologicamente all'avanguardia per formare i professionisti del futuro.",
-  },
-  {
-    title: 'Servizi al lavoro',
-    imageUrl:
-      'https://teamtrex.com.au/wp-content/uploads/2021/06/Strength-icon.png',
-    description:
-      "Orientamento, stage e alternanza, i nostri esperti ti accompagneranno fino all'effettivo inserimento nelle aziende del settore.",
-  },
-]
-
-// slides per il carousel
-let slides = [
-  {
-    titolo: 'Fatti un bel giro',
-    descrizione:
-      'Scopri tutti i segreti del nostro Centro con il tour virtuale!',
-    immagine:
-      'https://www.wallpapermania.eu/images/lthumbs/2013-01/4170_Love-between-animals-true-love.jpg',
-    colore: '#ED4C67',
-    colore2: 'rgba(100,100,100,0)',
-    opacity: 0.8,
-    blur: '0.5rem',
-    buttonText: 'Scopri di più!',
-    buttonUrl: 'https:...',
-  },
-  {
-    titolo: 'Concorso nazionale settore elettrico',
-    descrizione:
-      "A maggio il nostro Centro avrà l'onore di ospitare il Concorso Nazionale del Settore Elettrico: tutti i Centri di Formazione Professionale salesiani d'Italia invieranno i loro campioni per una settimana di sfida e condivisione professionale...",
-    colore: '#22aa22',
-  },
-]
-
-// Il nostro team
-let members = [
-  {
-    name: 'Martina Dianese',
-    role: 'Orientatrice',
-    description: 'And I love you like Kanye',
-    description1: 'loves Kanye. We need to restart the human foundation',
-    imageUrl: '/images/martina.png',
-    email: 'info@planyourlife.it',
-    phone: '0421 3388949874950',
-  },
-]
-
 let servizi = [
   {
     title: 'Orientamento Professionale',
     description:
       'Il Career Guidance è ciò che guida la persona nella presa di consapevolezza delle proprie abilità personali e competenze e lo sostiene nella ricerca attiva del lavoro. Attraverso strumenti propri dell’orientamento si costruisce un profilo professionale personalizzato, con i propri punti di forza e di debolezza, si chiarifica il tipo di lavoro ed azienda adeguato al tale profilo, quali sono le priorità lavorative e la propria concezione di lavoro. Si stabilisce insomma una tabella di marcia per il raggiungimento dei propri obiettivi e si acquisiscono le conoscenze necessarie per la crescita professionale Si viene affiancati nella stesura del cv, nella preparazione al colloquio di lavoro e nella sua ricerca attiva nel territorio.',
-    immagineUrl:
-      '/images/orientamento_professionale.jpg',
+    immagineUrl: '/images/orientamento_professionale.jpg',
   },
   {
     title: 'Orientamento Scolastico',
     description:
       'Come orientarsi nella scelta della scuola superiore? Quando iniziare a guardarsi intorno ? Come fare a capire se la scelta che sto facendo è quella giusta? Queste e molte altre sono domande che ogni giorno mi vengono rivolte e dare la risposta è il risultato di un percorso complesso , che porta bambini e studenti ad acquisire una corretta visione di sé, delle proprie potenzialità ed attitudini al fine di fare una scelta consapevole ed in linea con le proprie risorse personali. Questo tipo di attività aiuta non solo a chiarire le opzioni educative disponibili, ma può anche impedire che vengano fatte scelte sbagliate che potrebbero portare frustrazione e conseguente dispersione scolastica. I ragazzi vengono incoraggiati  ad esplorare le proprie capacità accademiche e non, aprendosi a nuove opportunità ed idee per arrivare alla costruzione di un progetto di vita ad hoc.',
-    immagineUrl:
-      '/images/orientamento_scolastico.png',
+    immagineUrl: '/images/orientamento_scolastico.png',
   },
   {
     title: 'Orientamento Universitario',
     description:
       'L’orientamento universitario è una tappa cruciale che concretizza sempre di più il percorso di vita della persona e pertanto deve essere fatta con adeguata consapevolezza ed attenzione. Non si tratta soltanto di scegliere una facoltà, ma si colloca all’interno del percorso di formazione continua che caratterizza ciascun individuo. E’ un fondamentale collegamento tra la scuola superiore ed il futuro professionale, pertanto deve assumere un adeguato significato. La finalità dell’orientamento universitario è la medesima di tutti i luoghi dell’orientamento: consentire alla persona di acquisire sempre maggior consapevolezza del proprio valore e del proprio essere.',
-    immagineUrl:
-      '/images/orientamento_universitario.png',
+    immagineUrl: '/images/orientamento_universitario.png',
   },
 ]
 
-const testimonials = [
+let strumenti = [
   {
-    name: 'Michele Venturato',
-    imageUrl:
-      'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1600',
-    text: "Grande scuola. Una vera scuola professionale e morale. Sforna ogni anno moltissimi tecnici aggiornati con I tempi e in grado di integrarsi con facilita' nel mondo del lavoro. Complimenti a tutto il sistema.",
-    social: '@Google',
+    title: 'Test psico-attitudinali e motivazioni',
+    description:
+      'I test psico-attitudinali sono strumenti utilizzati per valutare le attitudini, le capacità cognitive e le caratteristiche della personalità. Questi test possono includere diverse tipologie, come: test di intelligenza, di attitudine professionale, di personalità, di abilità specifiche, di motivazione e interesse.',
+    immagineUrl: '/images/orientamento_universitario.png',
   },
   {
-    imageUrl:
-      'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1600',
-    name: 'Daniele Mladenovic',
-    text: "Ho trascorso alcuni anni presso il CFP e ho trovato l'ambiente molto accogliente e confortevole, grazie alla disponibilità di tutto il personale. In particolare, ho apprezzato la passione degli insegnanti, che si sono dimostrati molto competenti. Inoltre, ho avuto l'opportunità di partecipare ad attività extracurricolari e progetti che mi hanno permesso di sviluppare le mie capacità. Infine, consiglio questo istituto sottolineando l'importanza dello sviluppo e della scoperta dei propri talenti che è ciò che ci rende unici.",
-    social: '@BOBTEK',
+    title: 'Questionari di autovalutazione',
+    description:
+      'I questionari di autovalutazione sono strumenti utili per riflettere su vari aspetti della propria vita, come le competenze professionali, le attitudini personali e le aree di sviluppo. Ecco alcuni tipi comuni di questionari di autovalutazione: autovalutazione delle competenze, della personalità, della carriera, del benessere, delle prestazioni.',
+    immagineUrl: '/images/orientamento_universitario.png',
   },
   {
-    imageUrl:
-      'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1600',
-    name: 'Pino West',
-    text: "Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione Qui c'è una belissima descrizione ",
-    social: '@PINOWEST',
+    title: 'E-Portfolio',
+    description:
+      'Un e-portfolio è uno strumento digitale che raccoglie e presenta una varietà di contenuti relativi alle competenze, esperienze e risultati di una persona. È utilizzato in contesti educativi e professionali per documentare e riflettere sui progressi personali e professionali, nonché per dimostrare competenze e realizzazioni a potenziali datori di lavoro, colleghi o istituzioni educative.',
+    immagineUrl: '/images/orientamento_universitario.png',
   },
-]
-
-let prodotti2 = [
   {
-    immagineUrl:
-      'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-    url: 'https://cinema.donboscosandona.it',
+    title: 'Questionario PerformanSe',
+    description:
+      'Il questionario PerformanSe è uno strumento di valutazione che permette di analizzare le caratteristiche professionali e comportamentali di una persona, evidenziando le sue motivazioni, potenzialità e aree di miglioramento. Questo strumento si basa su modelli psicologici consolidati e offre un\'analisi completa e personalizzata, utile per l\'orientamento professionale, la selezione del personale e lo sviluppo delle competenze.',
+    immagineUrl: '/images/orientamento_universitario.png',
+  },
+  {
+    title: 'Bilancio di competenze attraverso il metodo Retravailler',
+    description:
+      'E\' un percorso di orientamento professionale che aiuta le persone a identificare e valorizzare le proprie competenze, aspirazioni e potenzialità, al fine di definire un progetto professionale concreto e realizzabile. Questo metodo si basa su un approccio personalizzato e partecipativo, che mette al centro la persona e le sue risorse.',
+    immagineUrl: '/images/orientamento_universitario.png',
   },
 ]
 
@@ -251,57 +190,6 @@ let certifications = [
       'https://seeklogo.com/images/C/CERMET_SINCERT-logo-129ABB30BE-seeklogo.com.png',
     text1: 'Cod. A033 - Formazione Iniziale,',
     text2: 'Superiore e Orientamento',
-  },
-]
-
-let date = [
-  {
-    date: '15 Giugno',
-    afternoon: 'Inizio medie',
-    evening: 'Serata film',
-    immagineUrl:
-      'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-    url: '/elettrico',
-  },
-  {
-    date: '15 Giugno',
-    morning: 'Inizio elementari',
-    afternoon: 'Inizio medie',
-    evening: 'Serata film',
-    immagineUrl:
-      'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-    url: '/elettrico',
-    bgColor: '#ee5253',
-  },
-  {
-    date: '15 Giugno',
-    morning: 'Inizio elementari',
-    afternoon: 'Inizio medie',
-    immagineUrl:
-      'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-    url: '/elettrico',
-  },
-  {
-    date: '15 Giugno',
-    morning: 'Inizio elementari',
-    immagineUrl:
-      'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-    url: '/elettrico',
-  },
-  {
-    date: '15 Giugno',
-    morning: 'Inizio elementari',
-    afternoon: 'Inizio medie',
-    immagineUrl:
-      'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-    url: '/elettrico',
-  },
-  {
-    date: '15 Giugno',
-    morning: 'Inizio elementari',
-    immagineUrl:
-      'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-    url: '/elettrico',
   },
 ]
 
