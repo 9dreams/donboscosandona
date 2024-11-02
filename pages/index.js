108eaadc7885213331187bbed36d55fbe9f46d4e
dottorgross <77401433+dottorgross@users.noreply.github.com>
2024-11-02 16:47:14 +0100

    Versione iniziale vuota

---
+++ pages/index.js
@@ -0,0 +1,57 @@
+import { Container, Grid, Typography } from '@mui/material'
+import {
+  Carousel,
+  Certifications,
+  Layout,
+  LandingHero,
+  Features,
+  Products,
+  Calendar,
+  Testimonials,
+  Team,
+  Maps,
+  Paragraph,
+  Table,
+  News,
+  SwiperNews,
+  Featured,
+  NavBar,
+  Logos,
+  Sponsor,
+  Elements,
+} from '/components'
+
+export default function Home({ data, elementi }) {
+  return (
+    <Layout>
+      <LandingHero
+        opacity={0.5}
+        title='Progetto educativo'
+        description='Diventa adulto con il metodo educativo di don Bosco preparandoti ad entrare a testa alta nel mondo professionale di domani!'
+        buttonUrl='j'
+        buttonText='Guarda il video'
+        imageUrl='https://source.unsplash.com/random'
+      />
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  let res = await fetch(
+    'https://channels.donboscosandona.it/api/posts/inoratorio'
+  )
+  const data = await res.json()
+
+  res = await fetch(
+    'https://channels.donboscosandona.it/api/posts/donboscosandona_elements'
+  )
+  const elementi = await res.json()
+
+  return {
+    props: { data, elementi },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 minutes
+    revalidate: 1800, // In secondi: il build viene fatto al massimo una volta ogni mezzora
+  }
+}
