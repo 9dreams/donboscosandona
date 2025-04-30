f07be4d61e22eb83ee8baad89bf49131c8dd5948
Matteo Buccudu <m.buccudu@donboscosandona.it>
2025-04-30 10:07:05 +0200

    Favicon sito

--- cc/servizidisposizione.js
+++ cc/servizidisposizione.js
@@ -8,7 +8,7 @@ export default function Servizidisposizione({ servizi }) {
         I servizi a vostra disposizione
       </h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-12 gap-y-12">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-14 gap-y-12">
         {servizi.map((servizio) => (
           <Servizio
             immagine={servizio.imgSrc}
