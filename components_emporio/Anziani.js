887a9bb2a9608fba7f6e55a5f3a070d33eb8a1ea
Filippo Marcon <f.marcon@donboscosandona.it>
2025-05-12 13:17:19 +0200

    style: add missing empty line for better readability

--- components_emporio/Anziani.js
+++ components_emporio/Anziani.js
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import Link from "next/link";
+
 const scrollableContent = [
   {
     title: "Formazione per volontari",
